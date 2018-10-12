d11f37dee1259158ae1783459c394f2b4187f2ce
Jonathan <jonking93@email.arizona.edu>
2018-10-12 13:54:35 -0700

    Adding error checking

--- doffENSRF.m
+++ doffENSRF.m
@@ -1,4 +1,4 @@
-function[] = doffENSRF(M, D, R, H, obsDetails)
+function[A] = doffENSRF(M, D, R, H, obsDetails)
 %% Implements data assimilation for a (d)ynamic (off)line model ensemble. 
 % Uses an ensemble square root filter to perform updates.
 %
@@ -17,13 +17,15 @@ function[] = doffENSRF(M, D, R, H, obsDetails)
 %
 % DOES NOT handle overlapping sites (but eventually should...)
 %
-
-
-
-% M: Time-propagating (N x nEns x t)
 %
-% D: (nObs x t) If there are not observations at a site during a particular
-% time step, it should be marked with NaN.
+% ----- Inputs -----
+%
+% M: A 3D array of time-propagating model ensemble states. Each dim1 x dim2
+% matrix is an ensemble of column state vectors at a point in time. Time (dim3)
+% extends for the length of the observation averaging period. (N x nEns x tspan)
+%
+% D: The set of observations (both time-averaged and instantaneous)
+% through time. (nObs x t)
 %
 % R: (nObs x t)
 %
@@ -33,13 +35,7 @@ function[] = doffENSRF(M, D, R, H, obsDetails)
 % 
 
 % Do some error checking
-errCheck(M, D, R, H)
-
-% !!!!!
-% We should implement error checks here.
-
-
-
+errCheck(M, D, R, H, obsDetails)
 
 % Get the number of time steps
 nTime = length(sTime);
@@ -49,15 +45,10 @@ for t = 1:nTime
     
     % Get the indices of observations that are available in this time step.
     currObs = ~isnan( D(:,t) );
-    
-    % If there aren't any obs...
-    if ~any(currObs)
         
-        % The update is just the model prior.
-        A(:,:,t) = M(:,:,t);
-        
-    % But if there are obs...
-    else
+    % If there are obs, then we will update the model states.. If not, then
+    % the model prior will be the final state.
+    if any(currObs)
         
         % Get all the sets of current observations. This may include
         % instantaneous observations and time-averaged observations of
@@ -79,7 +70,7 @@ for t = 1:nTime
                 
                 % Then this is the set of instantaneous obs. Get the
                 % updated state.
-                A(:,:,t) = ensrfUpdate( M(:,:,t), D(currSet,t), R(currSet,t), H(currSet,:) );
+                M(:,:,t) = ensrfUpdate( M(:,:,t), D(currSet,t), R(currSet,t), H(currSet,:) );
                 
             % Otherwise, this is a time-average
             % !!!!!!
@@ -88,7 +79,7 @@ for t = 1:nTime
             else
                 
                 % So use a Dirren-Hakim update
-                A(:,:,t-nPrev:t) = dhUpdate( M(:,:,t-nPrev:t), D(currSet,t), T(currSet,t), H(currSet,:) );
+                M(:,:,t-nPrev:t) = dhUpdate( M(:,:,t-nPrev:t), D(currSet,t), T(currSet,t), H(currSet,:) );
             end
         end
     end
@@ -97,7 +88,7 @@ end
     
     
 % Does error checking
-function[] = errCheck(M, D, R, obsDetails)
+function[] = errCheck(M, D, R, H, obsDetails)
 
 if ndims(M) ~= 3 
     error('M must be a 3 dimensional array, N x nEns x time');
@@ -107,18 +98,43 @@ end
 
 if any(isInf(D))
     error('D should not contain Inf');
+elseif ~ismatrix(D)
+    error('D must be a matrix');
+end
+
+if size(D) ~= size(R)
+    error('D and R must have the same dimensions');
+elseif size(M,3)~=size(D,2)
+    error('The time dimensions in M and D are not of equal length.');
+elseif size(D,1) > size(M,1)
+    error('The number of observations cannot exceed the size of the state vector.')
 end
 
 if any(R(:)<=0)
     error('All values in R must be positive.');
 end
 
-if size(M,3)~=size(D,2)
-    error('The time dimension in M and D are not of equal length.');
-elseif size(M,3)~=size(R,2)
-    error('The time dimension in M and R are not of equal length.');
+if ~ismatrix(H) || ~islogical(H)
+    error('H must be a logical matrix');
+elseif size(H,1) ~= size(D,1)
+    error('H and D must have the same number of rows (observations)');
+elseif size(H,2) ~= size(M,1)
+    error('The number of columns in H must match the number of rows in M (state vector length)');
 end
 
+if any( sum(H,2)==0 )
+    x = find( sum(H,2)==0,1);
+    error( 'Observation %.f is not marked in the sampling matrix H',x);
+elseif any( sum(H,2)>1 )
+    x = find( sum(H,2)>1,1);
+    error('Observation %.f is marked in multiple locations in H',x);
+elseif any( sum(H,1)>1 )
+    x = find( sum(H,1)>1,1 );
+    overlap = find(H(:,x));    
+    error('Observations cannot overlap in space. Observations %.f and %.f overlap.',overlap(1),overlap(2));
+end    
+
+end
 
 
 
