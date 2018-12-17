8376923e4c967960f67a89c7b88116614478b8b9
JonKing93 <jonking93@gmail.com>
2018-12-17 08:53:22 -0700

    Removed old builds

--- DA Framework/Old Builds/Seasonal DA/Analysis pieces/updateA.m
+++
@@ -1,9 +0,0 @@
-function[Amean, Adev] = updateA(Mmean, K, innov, Mdev, alpha, Ydev)
-
-% Update the state vector mean
-Amean = Mmean + K*innov;
-
-% Update the state vector deviations
-Adev = Mdev - alpha' .* K * Ydev;
-
-end
