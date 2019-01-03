921eed91420005aa1f08ba106a31995f060471b7
JonKing93 <jonking93@gmail.com>
2019-01-03 13:15:16 -0700

    Added sequences

--- Dash DA/Build Ensemble/basicLoop.m
+++ Dash DA/Build Ensemble/basicLoop.m
@@ -6,30 +6,48 @@ M = NaN( nState, nEns );
 % Get an index cell to use for each ensemble member
 [ic, ix] = getFixedIndexCell( fixDex, fixed );
 
+% Get the sequence array
+seqArray = buildSequenceArray( fixed, seqDex );
+
+% Create a cell to hold the subscripts of each sequence
+currSeq = cell( sum(~fixed), 1 );
+
 % For each ensemble member
 for m = 1:nEns
     
-    % For each ensemble variable, get the index of the current ensemble
-    % member selection
-    for d = 1:nDim
-        if ~fixed(d)
-            ic{d} = ensDex(m,d) + meanDex{d};
+    % Preallocate the collection of all sequences
+    Mseq = NaN( nFixed, nSeq );
+    
+    % For each sequence
+    for s = 1:max(seqArray)
+        
+        % Get the index of the current sequence for each dimension
+        currSeq = getCurrSequence( fixed, seqArray, s );
+        
+        % Get the load indices for each ensemble variable
+        for d = 1:nDim
+            if ~fixed
+                ic{d} = ensDex(m,d) + seqDex{d}(currSeq(d)) + meanDex{d};
+            end
         end
-    end
+        
+        % Do the initial load from the .mat file
+        Mcurr = m.gridFile( ic{:} );
     
-    % Do the initial load from the .mat file
-    Mcurr = m.gridFile( ic{:} );
-    
-    % Restrict any fixed indices that were not equally spaced
-    Mcurr = Mcurr( ix{:} );
-    
-    % Take the mean in any relevant dimension
-    for d = 1:nDim
-        if takeMean(d)
-            Mcurr = mean( Mcurr, d, nanflag );
+        % Restrict any fixed indices that were not equally spaced
+        Mcurr = Mcurr( ix{:} );
+
+        % Take the mean in any relevant dimension
+        for d = 1:nDim
+            if takeMean(d)
+                Mcurr = mean( Mcurr, d, nanflag );
+            end
         end
+        
+        % Add to the collection of sequences
+        Mseq(:,s) = Mcurr(:);
     end
     
     % Add to the ensemble as a state vector
-    M(:,m) = Mcurr(:);
+    M(:,m) = Mseq(:);
 end    
---
+++ Dash DA/Build Ensemble/buildSequenceArray.m
@@ -0,0 +1,21 @@
+function[seqArray] = buildSequenceArray( fixed, seqDex )
+%% Build an array to subscript into each sequence
+
+% Initialize the array size
+arraySize = NaN( sum(~fixed),1);
+k = 1;
+
+% For each dimension that is an ensemble dimension
+nDim = numel(fixed);
+for d = 1:nDim
+    if ~fixed(d)
+        % Get the number of elements in each sequence
+        arraySize(k) = numel( seqDex{d} );
+        k = k + 1;
+    end
+end
+
+% Get the sequence array
+seqArray = reshape( 1:prod(arraySize), arraySize );
+
+end
