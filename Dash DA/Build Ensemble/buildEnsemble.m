4f593bf03e384c8a6222bfae7d9fe31807291a1d
JonKing93 <jonking93@gmail.com>
2019-01-03 22:57:50 -0700

    Multiple variables for buildEnsemble

--- Dash DA/Build Ensemble/buildEnsemble.m
+++ Dash DA/Build Ensemble/buildEnsemble.m
@@ -1,57 +1,69 @@
-function[M] = buildEnsemble( nEns, fixDex, seqDex, meanDex )
+function[M] = buildEnsemble( nEns, design )
+%% This is the basic loop that will build the ensemble
 
-%% This is the basic loop that will build the ensemble 
+% Get the state variable indices for each variable
+[varCell, nEls] = getVariableIndices( design );
+nState = sum(nEls);
+nVar = numel( design.varDesign );
 
 % Preallocate the ensemble
-M = NaN( nState, nEns );
+M = NaN( nState, nEns ); 
 
-% Get an index cell to use for each ensemble member
-[ic, ix] = getFixedIndexCell( fixDex, fixed );
+% For each variable
+for v = 1:nVar
 
-% Get the sequence array
-seqArray = buildSequenceArray( fixed, seqDex );
-
-% Create a cell to hold the subscripts of each sequence
-currSeq = cell( sum(~fixed), 1 );
-
-% For each ensemble member
-for m = 1:nEns
+    % Get the variable design
+    var = design.varDesign(v);
+    nDim = numel( var.dimID );
     
-    % Preallocate the collection of all sequences
-    Mseq = NaN( nFixed, nSeq );
+    % Get a read-only matfile
+    grid = matfile( var.file );
+
+    % Get an index cell to use for each ensemble member
+    [ic, ix] = getFixedIndexCell( var.fixDex, var.fixed );
+
+    % Get the sequence array
+    seqArray = buildSequenceArray( var.fixed, var.seqDex );
+
+    % For each ensemble member
+    for m = 1:nEns
     
-    % For each sequence
-    for s = 1:max(seqArray)
+        % Preallocate the collection of all sequences
+        Mseq = NaN( nFixed, numel(seqArray) );
+    
+        % For each sequence
+        for s = 1:max(seqArray)
         
-        % Get the index of the current sequence for each dimension
-        currSeq = getCurrSequence( fixed, seqArray, s );
+            % Get the index of the current sequence for each dimension
+            currSeq = getCurrSequence( var.fixed, seqArray, s );
         
-        % Get the load indices for each ensemble variable
-        for d = 1:nDim
-            if ~fixed
-                ic{d} = ensDex(m,d) + seqDex{d}(currSeq(d)) + meanDex{d};
+            % Get the load indices for each ensemble variable
+            for d = 1:nDim
+                if ~var.fixed
+                    ic{d} = var.ensDex(m,d) + var.seqDex{d}(currSeq(d)) + var.meanDex{d};
+                end
             end
-        end
         
-        % Do the initial load from the .mat file
-        Mcurr = m.gridFile( ic{:} );
+            % Do the initial load from the .mat file
+            Mcurr = grid.gridData( ic{:} );
     
-        % Restrict any fixed indices that were not equally spaced
-        Mcurr = Mcurr( ix{:} );
+            % Restrict any fixed indices that were not equally spaced
+            Mcurr = Mcurr( ix{:} );
 
-        % Take the mean in any relevant dimension
-        for d = 1:nDim
-            if takeMean(d)
-                Mcurr = mean( Mcurr, d, nanflag );
+            % Take the mean in any relevant dimension
+            for d = 1:nDim
+                if var.takeMean(d)
+                    Mcurr = mean( Mcurr, d, var.nanflag{d} );
+                end
             end
-        end
         
-        % Add to the collection of sequences
-        Mseq(:,s) = Mcurr(:);
+            % Add to the collection of sequences
+            Mseq(:,s) = Mcurr(:);
+        end
+
+        % Add to the ensemble
+        M( varCell{v}, m ) = Mseq(:);
     end
-    
-    % Add to the ensemble as a state vector
-    M(:,m) = Mseq(:);
-end    
+end
 
 end
