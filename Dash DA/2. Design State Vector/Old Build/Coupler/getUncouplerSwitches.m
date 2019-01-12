2986625b0c570cf868be99e796e4bfb31e840bc8
JonKing93 <jonking93@gmail.com>
2019-01-11 17:56:35 -0700

    Added uncoupler

---
+++ Dash DA/2. Design State Vector/Old Build/Coupler/getUncouplerSwitches.m
@@ -0,0 +1,22 @@
+function[toggle] = getUncouplerSwitches( state, ens, seq, mean )
+    
+% If ensemble is toggled, uncouple seq and mean as well
+if ens
+    ens = true;
+    seq = true;
+    mean = true;
+end
+    
+% If no toggles are activated, uncouple everything
+if ~state && ~ens && ~seq && ~mean
+    state = true;
+    ens = true;
+    seq = true;
+    mean = true;
+end
+
+toggle = [state, ens, seq, mean];
+
+end
+
+
