12bde43d3614e81f40554d7e104063e081f50b27
JonKing93 <jonking93@gmail.com>
2019-02-19 13:20:37 -0700

    Added energy statistic

--- 4. PSMs/Bias Correction/npdft.m
+++ 4. PSMs/Bias Correction/npdft.m
@@ -1,12 +1,41 @@
-function[Xs, R] = npdft( Xs, Xt, tol )
+function[X, E, jR, jXs] = npdft( Xs, Xt, tol, nIter )
+%% Performs the N-pdft algorithm.
+% Applies bias correction by equating the CDF of N-dimensional source data 
+% with an N-dimensional target. Uses the energy distance statistic to
+% monitor algorithm convergence.
+%
+% X = npdft( Xs, Xt, tol )
+% Applies the N-pdft alogrithm until the energy distance of the source and
+% target data falls below a threshold. Returns the bias-corrected output.
+%
+% X = npdft( Xs, Xt, tol, nIter )
+% Sets a maximum number of allowed iterations.
+%
+% [X, E] = npdft( ... )
+% Also returns the energy statistic for each iteration.
+%
+% [X, E, jR, jXs] = npdft( ... )
+% Also returns the rotation matrix and rotated Xs values for each iteration
+%
+% ----- Inputs -----
 %
 % Xs: Source data
 %
 % Xt: Target data
 %
-% tol: threshold to use for convergence
+% tol: threshold to use for convergence.
+%
+% nIter: A maximum number of iterations.
 %
 % ----- Outputs -----
+%
+% X: The bias corrected source data.
+%
+% E: The energy statistic for each rotation.
+%
+% jR: The rotation matrix used for each iteration.
+%
+% jXs: The rotated source data for each iteration.
 
 % Check that Xs and Xt are matrices
 if ~ismatrix(Xs) || ~ismatrix(Xt)
@@ -43,15 +72,22 @@ while ~converge
     Xs = rXs / R;
     
     % Get the squared energy distance
-    D2 = estat( Xs, Xt );
+    E = estat( Xs, Xt );
     
-    % Continue until the PDFs have converged
-    if D2 < tol
+    % Decrement
+    nIter = nIter - 1;
+    
+    % Continue until the PDFs have converged or the maximum number of
+    % iterations is reached.
+    if E < tol
+        converge = true;
+    elseif nIter <= 0
         converge = true;
     end
+     
 end
 
 % Restore the mean and standard deviation from the target
-Xs = Xs .* stdT + meanT;
+X = Xs .* stdT + meanT;
 
 end
