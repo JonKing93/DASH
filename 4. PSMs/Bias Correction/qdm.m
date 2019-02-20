fde526942f589f82a2152823df1d1f92cb58bf4b
JonKing93 <jonking93@gmail.com>
2019-02-20 11:14:52 -0700

    qdm doc

--- 4. PSMs/Bias Correction/qdm.m
+++ 4. PSMs/Bias Correction/qdm.m
@@ -1,6 +1,27 @@
 function[X] = qdm(Xo, Xm, Xp, type)
 %% Quantile delta mapping
 %
+% X = qdm(Xo, Xm, Xp)
+% Do quantile delta mapping while preserving relative changes.
+% 
+% X = qdm(Xo, Xm, Xp, type)
+% Specify whether to preserve relative or absolute change in quantiles.
+%
+% ----- Inputs -----
+%
+% Xo: Observed values
+%
+% Xm: Modeled historical values analogous to the observed values
+%
+% Xp: Model projected values
+%
+% type: A string flag
+%       'rel': Preserve relative changes in quantiles
+%       'abs': Preserve absolute changes in quantiles
+%
+% ----- Outputs -----
+%
+% X: The bias-corrected future values
 
 % Check that Xs and Xt and Xp are vectors
 if ~isvector(Xs) || ~isvector(Xt) || ~isvector(Xp)
@@ -37,4 +58,3 @@ elseif strcmpi(type, 'abs')
 end
 
 end
-
