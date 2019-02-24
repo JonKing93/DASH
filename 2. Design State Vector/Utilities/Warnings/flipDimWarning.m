6418f01d4a9e62b0cd224ddf62a793d8f1b94c60
JonKing93 <jonking93@gmail.com>
2019-02-23 20:43:14 -0700

    Made flipDimWarning less irritating

--- 2. Design State Vector/Utilities/Warnings/flipDimWarning.m
+++ 2. Design State Vector/Utilities/Warnings/flipDimWarning.m
@@ -15,10 +15,6 @@ function[] = flipDimWarning( dim, var, fromTo, coupVars )
 fprintf('Dimension %s of variable %s is being converted from a %s to a %s dimension. \n', ...
     dim, var, fromTo{1}, fromTo{2} );
 
-fprintf(['The variables: ', sprintf('%s, ', coupVars{:}), '\b\b are coupled to %s\n'], var);
-fprintf('Continuing will also convert %s in each of these variables to a %s dimension.\n',...
-         dim, fromTo{2});
+fprintf( ['\tConverting %s from %s to %s dimension for coupled variables: ', sprintf('%s, ', coupVars{:}), '\b\b\n'], dim, fromTo{1}, fromTo{2} );
 
-queryContinue('dimension conversion');
 end
-
