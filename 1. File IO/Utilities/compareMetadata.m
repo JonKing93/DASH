0252165a8613b9f3e01d33d11f9b2e5bdd071a72
JonKing93 <jonking93@gmail.com>
2019-04-09 09:25:37 -0700

    Added new compareMetadata

---
+++ 1. File IO/Utilities/compareMetadata.m
@@ -0,0 +1,29 @@
+function[] = compareMetadata( meta, m, field, indices )
+% Compares user-specified metadata to metadata in a .grid file at specific
+% indices.
+
+% Load the grid metadata
+gridMeta = m.meta;
+
+% Check that the grid metadata has values for the correct dimension'
+if ~isfield(gridMeta, field)
+    error('The metadata in the .grid file does not contain values for the %s dimension.', field);
+end
+
+% Get the values for the correct dimension
+gridMeta = gridMeta.(field);
+
+% Restrict to the indices
+gridMeta = gridMeta( indices, : );
+
+% If the metadata is a row vector with exactly the number of indices, convert to column.
+if isrow(meta) && length(meta)==numel(indices)
+    meta = meta';
+end
+
+% Compare the metadata
+if ~isequaln( meta, gridMeta )
+    error('The provided metadata values do not match the metadata values in the .grid file at the specified indices.');
+end
+
+end
