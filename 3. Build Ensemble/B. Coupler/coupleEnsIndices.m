1e8c95e177eb7bf53b3520c62c716ed5d55be097
JonKing93 <jonking93@gmail.com>
2019-04-09 17:26:52 -0700

    Added coupler interface

---
+++ 3. Build Ensemble/B. Coupler/coupleEnsIndices.m
@@ -0,0 +1,16 @@
+function[design] = coupleEnsIndices( design )
+
+% Get the sets of coupled variables
+coupVars = getCoupledVars( design.isCoupled );
+
+% Trim the ensemble indices for each variable to only allow complete sequences
+for v = 1:numel(design.var)
+    design.var(v) = trimEnsembleDims( design.var(v) );
+end
+
+% Restrict each set of coupled variables to matching metadata
+for cv = 1:numel(coupVars)
+    design = matchCoupledMeta( design, coupVars{cv} );
+end
+
+end
