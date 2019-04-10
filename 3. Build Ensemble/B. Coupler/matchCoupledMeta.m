96d682a90f38eebf1bf877f604ff8f97a0bfdb69
JonKing93 <jonking93@gmail.com>
2019-04-09 17:25:00 -0700

    Simplified and isolated the coupling process

---
+++ 3. Build Ensemble/B. Coupler/matchCoupledMeta.m
@@ -0,0 +1,69 @@
+function[design] = matchCoupledMeta( design, cv )
+%% Gets the most restrictive metadata for a set of coupled variables.
+%
+% design: stateDesign
+%
+% cv: Variable indices of coupled variables
+%
+% ensDim: Name of ensemble dimensions
+%
+% ----- Written By -----
+% Jonathan King, University of Arizona, 2019
+
+% Get the first variable for quick reference
+var1 = design.var( cv(1) );
+
+% Get the ensemble dimensions
+ensDim = var1.dimID( ~var1.isState );
+
+% For each ensemble dimension
+for dim = 1:numel(ensDim)
+    
+    % Initialize the metadata with the values from the first variable
+    d = checkVarDim( var1, ensDim(dim) );
+    meta = var1.meta.(dim)( var1.indices{d} );
+    
+    % Go through the remaining variables
+    for v = 2:numel(cv)
+        varC = design.var( cv(v) );
+        
+        % Get the index of the dimension in the variable
+        d = checkVarDim( varC, ensDim(dim) );
+        
+        % Get the metadata for the current variable
+        currMeta = varC.meta.(dim)( varC.indices{d} );
+        
+        % Get the union (preserving inherent dimensionality)
+        meta = unionMetadata( meta, currMeta );
+    end
+    
+    % Throw an error if no metadata remains
+    if isempty(meta)
+        overlapError(design, cv, dim);
+    end
+
+    % Then run through each coupled variable
+    for v = 1:numel(cv)
+        
+        % Get the index of each ensemble dimension
+        varC = design.var( cv(v) );
+        d = checkVarDim( varC, ensDim(dim) );
+        
+        % Get the metadata for the variable
+        currMeta = varC.meta.(dim)( varC.indices{d} );
+
+        % And restrict indices to those matching the final metadata union.
+        [~, design.var( cv(v) ).indices] = unionMetadata( currMeta, meta );
+    end
+end
+
+end
+
+function[] = overlapError(design, cv, dim)
+
+coupled = sprintf('%s, ', design.var(cv).name);
+error( ['There is no overlapping metadata for the %s dimension of coupled variables: %s', ...
+        'so they cannot be coupled.', newline, ...
+        'Use the functions "metaGridfile.m" and "rewriteGridMetadata.m" to ensure that metadata is in a common format.'], ...
+        dim, coupled );
+end
