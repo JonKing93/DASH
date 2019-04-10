a6a23630ae135285d41c06eb7a69dbca69562bdc
JonKing93 <jonking93@gmail.com>
2019-04-10 11:16:03 -0700

    Huge improvement to the drawEnsemble functions. So much simplified

--- 3. Build Ensemble/D. Draw Ensemble/drawEnsemble.m
+++ 3. Build Ensemble/D. Draw Ensemble/drawEnsemble.m
@@ -1,110 +1,92 @@
-function[sampDex, dimID] = drawEnsemble( coupVars, nEns, overlap )
-%% Draws the ensemble indices for a set of coupled variables.
-%
-% coupVars: coupled varDesigns
-%
-% nEns: Ensemble size
-%
-% overlap: True/False for whether overlap is allowed
-%
-% ----- Written By -----
-% Jonathan King, University of Arizona, 2019
+function[design] = drawEnsemble( design, nDraws, coupVars )
 
 % For each set of coupled variables
 for cv = 1:numel(coupVars)
     vars = design.var( coupVars{cv} );
     
-    % Get the total number of possible draws
-    nDraws = totalDraws( vars(1) );
+    % Get the overlap permission
+    overlap = vars(1).overlap;
     
-    % Get the subscripted sequence elements
-    subSeq = subSequenceEls( vars );
+    % Get the index and size of each ensemble dimension
+    ensDim = find( ~var(1).isState );
+    [~, dimSize] = getVarIndices( vars(1) );    
+    ensSize = dimSize( ensDim );
     
-    
-    
-    %%%%%%%%%
-    
-    
-    
+    % Get the total number of possible draws, and index the draws that have
+    % NOT yet been selected.
+    nTot = totalDraws( vars(1) );
+    undrawn = (1:nTot)';
 
-
-
-
-
-
-
-
-% Get the ensemble dimensions
-ensDim = find( ~coupVars(1).isState );
-dimID = coupVars(1).dimID(ensDim);
-nDim = numel(ensDim);
-
-% Get the set of all possible draws
-[allDraws, ensSize] = getAllCombIndex( coupVars(1).indices(ensDim) );
-
-% Get the sequence elements
-if ~overlap
-    seqEls = getAllSequenceEls( coupVars, dimID );
-else
+    % Get the subscripted sequence elements. (These will be used to check
+    % for overlap between draws.) If overlap is permitted, just set to zero
     seqEls = zeros(1,nDim);
+    if ~overlap
+        seqEls = subSequenceEls( vars );
+    end
+    nSeq = size( seqEls, 1 );
+    
+    % Preallocate the sampling indices. These are the actual grid indices
+    % from which climate data will be loaded.
+    sampDex = NaN( nSeq*nDraws, numel(ensDim) );
+    
+    % Make draws until either the ensemble is built, or everything has been attempted
+    while nDraws > 0
+        if nDraws > numel(undrawn)
+            ensSizeError(nDraws, numel(undrawn));
+        end
+
+        % Randomly draw ensemble members
+        currDraws = randsample( undrawn, nDraws );
+
+        % Remove these draws from the list of undrawn ensemble members
+        undrawn( ismember( undrawn, currDraws) ) = [];
+
+        % Subscript the draws to N-dimensions.
+        subDraws = subdim( ensSize, currDraws );
+        
+        % Previously, ensemble indices were matched (via metadata) for the set
+        % of coupled variables. So, the subscripted draws refer to the
+        % same set of ensemble indices for all the coupled variables.
+        %
+        % Note that the subscripted draws refer to ensemble INDICES. They
+        % are indexing an index, and not a climate data value. This is
+        % good, because we want to check if the draw indices will overlap.
+        %
+        % So, we'll need to get the set of ensemble indices associated with
+        % these subscript indices, add in the sequence elements, and check
+        % for repeated elements for the full set of sampling indices.
+        
+        % Preallocate the set of newly drawn ensemble indices
+        ensDex = NaN( size(subDraws) );
+        
+        % Get the set of drawn ensemble indices.
+        for d = 1:numel(ensDim)
+            ensDex(:,d) = vars(1).indices{ ensDim(d) }( subDraws(:,d) );
+        end
+         
+        % Replicate the sequence elements over each drawn ensemble index
+        repSeqEls = repmat( seqEls, [nDraw,1] );
+        
+        % Replicate the drawn ensemble indices over each sequence element.
+        repEnsDex = repmat( ensDex(:)', [nSeq, 1] );
+        repEnsDex = reshape( repEnsDex, [nSeq*nDraws, nDim] );
+        
+        % Add together to get the full set of sampling indices associated 
+        % with these draws. Add to the set of previous successful draws.
+        sampDex( (end - nDraw*nSeq + 1):end, : ) = repEnsDex + repSeqEls;
+        
+        % Remove any draws associated with overlapping sampling indices.
+        sampDex = removeOverlappingDraws( sampDex, nSeq );
+        
+        % Get the number of draws remaining
+        nDraws = sum( isnan(sampDex(:,1)) / nSeq );
+    end
+        
+    % Get the final drawn ensemble indices
+    ensDraws = sampDex( 1:nSeq:end, : ) - seqEls(1,:);
+    
+    % Add the draws to the design
+    design.var( coupVars{cv} ) = assignEnsDraws( vars, ensDraws, vars(1).dimID(ensDim), undrawn ); 
 end
 
-% Preallocate the sampling indices
-nSeq = size(seqEls,1);
-sampDex = NaN(nSeq*nEns, nDim);
-
-% Initialize the number of draws
-nDraw = nEns;
-
-% Initialize while loop toggles
-timeOut = 1000;
-
-% Until all the draws are made...
-while nDraw > 0
-    
-    % If the method timed out, ask the user to continue
-    timeOut = timeOut-1;
-    if timeOut < 0
-        userContinueQuery(nEns);
-        timeOut = Inf;
-    end
-    
-    % Check that the draws can be made
-    if nDraw > numel(allDraws)
-        error('Cannot draw %0.f non-overlapping ensemble members. Use a smaller ensemble.',nEns);
-    end
-    
-    % Make random draws from the ensemble members
-    newDraws = randsample( allDraws, nDraw );
-    
-    % Subscript to grid dimensions
-    subDraws = subdim( ensSize, newDraws );
-    
-    % Index from draws to ensemble indices
-    ensDex = NaN( size(subDraws) );
-    for k = 1:nDim
-        ensDex(:,k) = coupVars(1).indices{ensDim(k)}( subDraws(:,k) );
-    end
-
-    % Get sequence elements for each draw
-    currSeq = repmat( seqEls, [nDraw,1]);
-    currDraw = reshape(  repmat(ensDex(:)', [nSeq,1]), [nSeq*nDraw, nDim] );
-    
-    % Get the full set of data indices
-    sampDex( (end - nDraw*nSeq + 1):end, : ) = currSeq + currDraw;
-    
-    % Erase repeat indices with NaN and move to the end of the sampling indices
-    sampDex = eraseRepeat( sampDex, nSeq );
-    
-    % Get the number of remaining draws
-    nDraw = sum(isnan(sampDex(:,1))) / nSeq;
-    
-    % Remove the current draws from the set of possible future draws
-    [~,drawn] = ismember(newDraws, allDraws);
-    allDraws(drawn) = []; 
 end
-
-% Only return the sequence reference indices
-sampDex = sampDex(1:nSeq:end,:) - seqEls(1,:);
-    
-end  
