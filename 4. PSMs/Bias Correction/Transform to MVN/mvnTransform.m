50d75ebb9155f9d9e3a6c6725d2aae39c8a68f57
JonKing93 <jonking93@gmail.com>
2019-05-07 17:08:15 -0700

    Added MVN interface

--- 4. PSMs/Bias Correction/Transform to MVN/mvnTransform.m
+++ 4. PSMs/Bias Correction/Transform to MVN/mvnTransform.m
@@ -1,23 +1,49 @@
-function[] = mvnTransform
-%% This transforms the columns of a matrix to multivariate normal.
+function[] = mvnTransform( X, transform, params )
+%% This transforms the columns of a matrix to multivariate normal using
+% various specified transformations.
+%
+% Xmvn = mvnTransform( X, transform, params )
+%
+% ----- Inputs -----
 %
 % X: A data matrix. Each column is a variable (nSample x nVar)
 %
 % transform: A string vector holding the transformation type for each
 %            variable. (1 x nVar)
 %
+%    Supported transforms:
+%        'boxcox': Extended box-cox
+%        'none': No transform is applied
+%
 % params: A cell array holding any parameters needed for transformation 
-%         {1 x nVar
+%         {1 x nVar}
+%       
+%      Parameter types:
+%        'boxcox': params = {lambda}
+%                       lambda: The box-cox parameter. (1 x 1)
+%
+%        'none': params = {[]}
+%
+%
+% ----- Outputs -----
+%
+% Xmvn: The transformed data matrix. (nSample x nVar)
+
+% ----- Written By -----
+% Jonathan King, University of Arizona, 2019
+
+% Error checking. Convert transforms to string array
+transform = errCheck(X, transform, params);
 
 % For each variable, apply the appropriate transformation
-for v = 1:nVar
+for v = 1:size(X,2)
     
     % Error check the parameters
-    checkParams( params(v), transform(v) );
+    checkNumParams( transform(v), params(v) );
     
     % Box-Cox
-    if strcmp( transform(v), "ebox" )
-        lambda = params(v){1};
+    if strcmp( transform(v), "boxcox" )
+        lambda = params{v}{1};
         X(:,v) = extBoxCox( X(:,v), lambda );
         
     % None
@@ -28,8 +54,45 @@ for v = 1:nVar
 end
 
 end
+
+function[] = checkNumParams( transform, params )
+
+% Box Cox
+if strcmp(transform, "boxcox")
+    if numel(params) ~= 1
+        error('The boxcox transform has a single parameter.');
+    end
+    
+% No transformation
+elseif strcmp(transform, "none") || strcmp(transform, "")
+    if numel(params) > 1 || ~isempty(params{1})
+        error('Parameters were specified for a null transformation.');
+    end
+end
+
+end 
+
+function[transform] = errCheck(X, transform, params)
         
+    if ~ismatrix(X)
+        error('X must be a matrix.');
+    end
+    N = size(X,2);
 
- 
+    if iscellstr(transform)
+        transform = string(transform);
+    end
 
-
+    if ~isvector(transform) || ~isstring(transform) || length(transform)~=N
+        error('transform must be a string vector with one element for each column of X.');
+    end
+    
+    supported = ["boxcox","","none"];
+    if any( ~ismember( transform, supported ) )
+        error('Unrecognized transform');
+    end
+    
+    if ~isvector(params) || ~iscell(params) || length(params)~=N
+        error('params must be a cell vector with one element for each column of X.');
+    end
+end
