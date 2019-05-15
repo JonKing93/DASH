15da7c7749379b6bfa4390e29853463620cb87b2
JonKing93 <jonking93@gmail.com>
2019-05-15 16:08:36 -0700

    first build of bilinear

--- 4. PSMs/Linear PSMs/bilinearPSM.m
+++ 4. PSMs/Linear PSMs/bilinearPSM.m
@@ -1,49 +1,67 @@
-% This implements a linear, univariate PSM
+% This implements a linear, bivariate PSM
 
 % ----- Written By -----
 % Jonathan King, University of Arizona, 2019
-classdef unilinearPSM < PSM
+classdef bilinearPSM < PSM
     
     properties
-        slope;
+        slope1;
+        slope2;
         intercept;
         
         coord;
         
         addUnit;
         multUnit;
+        
+        nVar1;
+        nVar2;
     end
     
     methods
         % Constructor
-        function obj = unilinearPSM( slope, intercept, lat, lon, varargin )
-
+        function obj = bilinearPSM( slope1, slope2, intercept, lat, lon, varargin )
+            
             % Check that slope and intercept are scalars
-            if ~isscalar(slope) || ~isscalar(intercept)
+            if ~isscalar(slope1) || ~isscalar(slope2) || ~isscalar(intercept)
                 error('slopes and intercepts must be scalars.');
 
             % lat and lon should be scalars
             elseif ~isscalar(lat) || ~isscalar(lon)
                 error('lat and lon must be scalars.');
             end
-
+            
             % Read in the optional unit conversion inputs
             [add, mult] = parseInputs( varargin, {'addUnit','multUnit'}, {0, 1}, {[],[]} );
 
             % Save
-            obj.slope = slope;
+            obj.slope1 = slope1;
+            obj.slope2 = slope2;
             obj.intercept = intercept;
             obj.coord = [lat, lon];
             obj.addUnit = add;
             obj.multUnit = mult;
         end
         
-        % State indices. Will take the mean of all state indices
-        function[] = getStateIndices( obj, ensMeta, varName, varargin )
-            obj.H = getClosestLatLonIndex( obj.coord, ensMeta, varName, varargin{:} );
+        % State indices.
+        function[] = getStateIndices( obj, ensMeta, var1, var1Args, var2, var2Args )
+            
+            % State indices for the first variable.
+            H1 = getClosestLatLonIndex( obj.coord, ensMeta, var1, var1Args{:} );
+            
+            % State indices for the second variable. (Can allow separate
+            % mean values for ensemble metadata)
+            H2 = getClosestLatLonIndex( obj.coord, ensMeta, var2, var2Args{:} );
+            
+            % Record the number of each variable
+            obj.nVar1 = numel(H1);
+            obj.nVar2 = numel(H2);
+            
+            % Concatenate to get final indices
+            obj.H = [H1; H2];
         end
-        
-        % Error check the PSM
+            
+        % Review PSM
         function[] = reviewPSM( obj )
             if isempty( obj.H )
                 error('State indices were not generated.');
@@ -55,13 +73,13 @@ classdef unilinearPSM < PSM
                 error('This PSM has properties with NaN elements.');
             end
         end
-        
-        % Convert units
+            
+        % Convert Units
         function[M] = convertUnits( obj, M )
             M = M + obj.addUnit;
             M = M .* obj.multUnit;
         end
-            
+        
         % Run the PSM
         function[Ye] = runPSM( obj, M, ~, ~ )
             
@@ -71,11 +89,16 @@ classdef unilinearPSM < PSM
             % Bias correct
             M = obj.biasCorrect(M);
             
-            % Take the mean if multiple variables are sampled
-            M = mean(M,1);
+            % Get the indices of elements associated with each variable
+            index1 = 1:obj.nVar1;
+            index2 = obj.nVar1 + (1:obj.nVar2);
             
-            % Do the linear model
-            Ye = M .* obj.slope + obj.intercept;
+            % Take any mean over the variables
+            X1 = mean( M( index1, :), 1 );
+            X2 = mean( M( index2, :), 1 );
+            
+            % Apply the linear relationship
+            Ye = (X1 .* obj.slope1) + (X2 .* obj.slope2) + obj.intercept;
         end
     end
 end
