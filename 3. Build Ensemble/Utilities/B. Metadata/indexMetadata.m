2618fa147e8c020f6f1b4f6d4f6df01d1f06d1f1
JonKing93 <jonking93@gmail.com>
2019-06-03 12:38:06 -0700

    Killed Nd indexer

--- 3. Build Ensemble/Utilities/B. Metadata/indexMetadata.m
+++
@@ -1,8 +0,0 @@
-function[meta] = indexMetadata( meta, index )
-%% Samples n-dimensional metadata from the indexed rows
-
-metaDex = repmat( {':'}, [1, ndims(meta)] );
-metaDex{1} = index;
-meta = meta( metaDex{:} );
-
-end
