5b0a1e983ef620df14f9c99306a15f093f8db704
jesstierney <jess.tierney@gmail.com>
2019-07-19 09:45:24 -0700

    update to ukPSM to make sure polygons are working correctly.

--- 4. PSMs/Specific Forward Models/UK 37/ukPSM.m
+++ 4. PSMs/Specific Forward Models/UK 37/ukPSM.m
@@ -128,12 +128,17 @@ classdef ukPSM < PSM
             % North Pacific polygon: Jun-Aug
             poly_p_lat=[45; 70; 70; 52.5; 45];
             poly_p_lon=[135; 135; 250; 232; 180];
-            
-            if inpolygon(obj.coord(2),obj.coord(1),poly_m_lon,poly_m_lat)
+            % Convert to -180 to 180 for Med. and Atl.
+            lon180=obj.coord(2);
+            lon180(lon180>180)=lon180(lon180>180)-360;
+            % Convert to 0 to 360 for Pacific
+            lon360=obj.coord(2);
+            lon360(lon360<0)=360+lon360(lon360<0);     
+            if inpolygon(lon180,obj.coord(1),poly_m_lon,poly_m_lat)
                 ind = [1 2 3 4 5 11 12];             
-            elseif inpolygon(obj.coord(2),obj.coord(1),poly_a_lon,poly_a_lat)
+            elseif inpolygon(lon180,obj.coord(1),poly_a_lon,poly_a_lat)
                 ind = [8 9 10];
-            elseif   inpolygon(obj.coord(2),obj.coord(1),poly_p_lon,poly_p_lat)
+            elseif   inpolygon(lon360,obj.coord(1),poly_p_lon,poly_p_lat)
                 ind = [6 7 8];
             else
                 ind = (1:12)';
