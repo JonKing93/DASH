6f33941e3f0ecae15070e58d635495a80b633d79
JonKing93 <jonking93@gmail.com>
2019-08-06 11:52:42 -0700

    Created class folder

---
+++ 2. Design State Vector/Design classes/@stateDesign/stateDesign.m
@@ -0,0 +1,100 @@
+classdef stateDesign
+    % stateDesign
+    % Stores design specifications and parameters for a state vector
+    % containing multiple variables.
+    %
+    % stateDesign Properties:
+    %   name - An identifying name for the state vector
+    %
+    %   var - An array of variable designs. These contain the design
+    %        specifications for how data should be read from .grid files
+    %        into a state vector.
+    %
+    %   varName - The names of the variables in the state design.
+    %
+    %   isCoupled - Notes whether variables are coupled. Coupled variables
+    %              are selected from the same ensemble indices for each
+    %              ensemble member.
+    %
+    %   autoCouple - Notes whether a variable should automatically be
+    %               coupled to new variables. Default is true.
+    %   
+    % stateDesign Methods:
+    %    stateDesign - Creates a new stateDesign object.
+    %    addVar - Adds a variable to the state vector
+    %    edit - Edits the design specifications for a variable.
+    %    copy - Copies design specifications from a template variable to
+    %           other variables.
+    %    disp - Displays information about the state vector.
+    %    couple - Couples specified variables.
+    %    uncouple - Uncouples specified variables.
+    %    remove - Removes a variable from the state vector.
+    
+    % ----- Written By -----
+    % Jonathan King, University of Arizona, 2019
+    
+    properties (SetAccess = private)
+        name;       % An identifier for the state vector.
+        var;        % The array of variable designs
+        varName;    % The names of the variables in the design.
+        isCoupled;  % Notes whether variables are coupled
+        autoCouple;  % Whether the variable should be automatically coupled to new variables.
+    end
+    
+    % Constructor block.
+    methods
+        function obj = stateDesign( name )
+            % Constructor for a stateDesign object.
+            %
+            % obj = stateDesign( name )
+            % Creates an empty stateDesign object and assigns an
+            % identifying name.
+            %
+            % ----- Inputs -----
+            %
+            % name: An identifying name for the stateDesign. Either a
+            %       string scalar or character row vector.
+            %
+            % ----- Outputs -----
+            % 
+            % obj: The new, empty stateDesign object.
+            
+            % Check that the name is allowed
+            if ~isstrflag(name)
+                error('The stateDesign name must be a string.');
+            end
+            
+            % Set the name
+            obj.name = name;
+            
+            % Initialize logical arrays
+            obj.isCoupled = logical([]);
+            obj.autoCouple = logical([]);
+        end
+    end
+    
+    % User methods.
+    methods
+        
+        % Adds a new variable to the state vector.
+        obj = addVar( obj, varName, file, autoCouple );
+        
+        % Edits the design specifications of a variable in the state vector.
+%         obj = edit( obj, varName, dim, dimType, varargin );
+        
+        % Copies indices from one variable to other variables.
+%         obj = copy( obj, fromVar, toVars );
+        
+        % Displays information about the state vector
+%         obj = disp( obj, varName, dim, longform );
+        
+        % Couples specified variables.
+%         obj = couple( obj, varNames, varargin );
+        
+        % Uncouples specified variables.
+%         obj = uncouple( obj, varNames, varargin );
+        
+        % Removes a variable from the state vector.
+%         obj = remove( obj, varName );        
+    end
+end
