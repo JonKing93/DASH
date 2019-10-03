28eb2467dca2cb235e922982f30b01710bc1e8b4
JonKing93 <jonking93@gmail.com>
2019-10-03 10:32:12 -0700

    Began creating dash object

---
+++ @dash2/dash2.m
@@ -0,0 +1,100 @@
+classdef dash2 < handle
+    % Implements data assimilation
+    
+    % ----- Written By -----
+    % Jonathan King, University of Arizona, 2019
+    
+    properties
+        settings;   % Instructions for different types of DA
+        
+        M;      % An ensemble. Either an ensemble object or (nState x nEns) matrix
+        D;      % Observations. 
+        R;      % Observation uncertainty.
+        F;      % Forward operators
+        
+        nState;
+        nEns;
+        nObs;
+        nTime;
+    end
+    
+    % Constructor
+    methods
+        function obj = dash2(M, D, R, F)
+            
+            % Create the default settings structure
+            obj.settings = struct('ensrf', [], 'particleFilter', [], 'optimalSensor', []);
+            obj.settings.ensrf = struct( 'serial', false, 'append', false, 'inflate', 1, 'localize', false, 'radius', NaN );
+            obj.settings.particleFilter = struct('type', "weight", 'N', NaN, 'big', false, 'nEns', NaN);
+            obj.settings.optimalSensor = struct('replace', true, 'nSensor', 1, 'sites', [], 'radius', 0 );
+            
+        end
+    end
+    
+    
+    % Settings for different types of analyses
+    methods
+        ensrfSettings( obj, varargin );
+        pfSettings( obj, varargin );
+        sensorSettings( obj, varargin );
+    end
+    
+    % User methods to begin analyses
+    methods
+        optimalSensor;
+        
+    
+    
+    
+    
+    
+    end
+    
+    
+    
+    % User methods
+    methods (Static)
+        
+        % Specifies to use localization
+        localize;
+        
+        % Specifies to inflate
+        inflate;
+        
+        % Uses an ensemble square root Kalman filter
+        ensrf;
+        
+        % Uses a particle filter
+        particleFilter;
+        
+        % Applies a particle filter to a data set too large to fit into
+        % active memory
+        bigParticleFilter;
+        
+        % Tests for optimal sensor placement
+        optimalSensor;
+    end
+    
+    % Internal methods
+    methods
+        
+        % Actually applies inflation
+        inflateEnsemble;
+        
+        % Computes localization weights
+        [w, yloc] = covLocalization;
+        
+        % Breaks apart an ensemble, get mean/deviations/variance
+        decomposeEnsemble;
+        
+        % 
+    end
+        
+        
+        
+        
+    
+        
+end
+        
+        
