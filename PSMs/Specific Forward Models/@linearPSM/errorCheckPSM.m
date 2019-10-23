70ac92d2ce15e3b69d5bce5c8bff23e77a3ebdfd
JonKing93 <jonking93@gmail.com>
2019-10-23 11:50:27 -0700

    Fixed linearPSM error checking

--- PSMs/Specific Forward Models/@linearPSM/errorCheckPSM.m
+++ PSMs/Specific Forward Models/@linearPSM/errorCheckPSM.m
@@ -1,11 +1,13 @@
 function[] = errorCheckPSM( obj )
 % Error checks a linear PSM
-if isempty(obj.slope) || isempty(obj.intercept) || isempty(obj.nH)
-    error('The slope, intercept and nH cannot be empty.');
-elseif any(isnan(obj.slope)) || any(isnan(obj.intercept)) || any(isnan(obj.varDex))
-    error('The slope, intercept, and nH cannot be NaN.');
-elseif ~isvector(obj.slope) || ~isvector(obj.varDex) || length(obj.slope)~=length(obj.varDex)
-    error('The slope and nH must be vectors with the same number of elements.');
+if isempty(obj.slopes) || isempty(obj.intercept) || isempty(obj.Hlim)
+    error('The slope, intercept and Hlim cannot be empty.');
+elseif any(isnan(obj.slopes)) || any(isnan(obj.intercept)) || any(isnan(obj.Hlim))
+    error('The slope, intercept, and Hlim cannot be NaN.');
+elseif ~ismatrix( obj.Hlim ) || size(obj.Hlim,2)~=2
+    error('Hlim must be a 2-column matrix.');
+elseif ~isvector(obj.slopes) || length(obj.slopes) ~= size(obj.Hlim,1)
+    error('slope and be a vector with the same number of elements as rows in Hlim.');
 elseif ~isscalar(obj.intercept)
     error('The intercept must be a scalar.');
 end
