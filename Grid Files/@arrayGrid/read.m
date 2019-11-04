d442be2b012a36a7019e8809b1b1dfff9ab46486
JonKing93 <jonking93@gmail.com>
2019-11-04 13:39:09 -0700

    removed old start referecne

--- Grid Files/@arrayGrid/read.m
+++ Grid Files/@arrayGrid/read.m
@@ -1,13 +1,18 @@
-function[X] = read(obj, start, count, stride, gridpath )
+function[X, m] = read(obj, scs, gridpath, passVal )
 %% Read data from an array saved locally to a .grid file.
 
-nDim = length(start);
+nDim = size(scs,2);
 loadIndex = cell(nDim,1);
 for d = 1:nDim
-    loadIndex{d} = start(d) : stride(d) : start(d)+stride(d)*(count(d)-1);
+    loadIndex{d} = scs(1,d): scs(3,d) : scs(1,d)+scs(3,d)*(scs(2,d)-1);
 end
 
-m = matfile( gridpath );
+if isempty( passVal )
+    m = passVal;
+else
+    m = matfile( gridpath );
+end
+
 X = m.(obj.dataName)( loadIndex{:} );
 
 end
