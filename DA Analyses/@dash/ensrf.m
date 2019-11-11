08c956ec7799f27b958cb54af53ef9daa4053594
JonKing93 <jonking93@gmail.com>
2019-11-11 10:12:42 -0700

    Reorganizing

---
+++ DA Analyses/@dash/ensrf.m
@@ -0,0 +1,84 @@
+function[output] = ensrf( obj )
+% Runs an ensemble square root Kalman filter
+%
+% output = obj.ensrf
+%
+% ---- Outputs -----
+%
+% output: A structure that may contain the following fields
+%
+%   settings - The settings used to run the filter
+%
+%   Amean - The updated ensemble mean (nState x nTime)
+%
+%   Avar - Updated ensemble variance (nState x nTime)
+%
+%   Ye - Proxy estimates
+%           Joint Updates:  (nObs x nEns)
+%           Serial Updates: (nObs x nEns x nTime)
+%
+%   Yi - Initial proxy estimates when using the appended Ye method. (nObs x nEns)
+%
+%   calibRatio - The calibration ratio. (nObs x nTime)
+%
+%   R - The observation uncertainty used to run the filter. Includes
+%       dynamically generated R values. (nObs x nTime).
+%
+%   sites - A logical array indicating which sites were used to update each
+%           time step. (nObs x nTime)
+
+% Get settings. Load the ensemble if necessary
+set = obj.settings.ensrf;
+M = obj.M;
+if isa(M,'ensemble')
+    M = M.load;
+end
+
+
+% Inflate ensemble
+M = dash.inflate( M, set.inflate );
+
+
+% Serial updates
+if strcmp(set.type, 'serial')
+    
+    % Default localization
+    w = set.localize;
+    if isempty(w)
+        w = ones( obj.nState, obj.nObs );
+    end 
+    
+    % Optionally append Ye
+    F = obj.F;
+    if set.append
+        [M, F, Yi] = dash.appendYe( M, F );
+    end
+    
+    % Do the updates
+    output = dash.serialENSRF( M, obj.D, obj.R, F, w );
+    
+    % Unappend if necessary
+    output.Append = false;
+    if set.append
+        dash.unappendYe;
+        output.Yi = Yi;
+        output.Append = true;
+    end
+    
+% Joint updates
+else
+    
+    % Default localization
+    if isempty(set.localize)
+        w = ones(obj.nState, obj.nObs);
+        yloc = ones(obj.nObs, obj.nObs);
+    else
+        w = set.localize{1};
+        yloc = set.localize{2};
+    end
+    
+    % Do the updates
+    output = dash.jointENSRF( M, obj.D, obj.R, obj.F, w, yloc, set.meanOnly );
+end
+
+end
---
+++ DA Analyses/@sensorTest/run.m
@@ -0,0 +1,21 @@
+function[output] = run( obj )
+% Does an optimal sensor test for a specific sensorTest object.
+%
+% output = obj.run
+%
+% ----- Outputs -----
+%
+% output: A structure with the following fields
+%
+%   settings - Settings used to run the analysis
+%
+%   bestH - The state vector indices of the best sites
+%
+%   bestSites - The index of the best sites in the sensorSites object
+%
+%   skill - The relative reduction in J variance of each placement.
+
+% Run
+output = dash.optimalSensor( obj.M, obj.Fj, obj.S, obj.nSensor, obj.replace, obj.radius );
+
+end
