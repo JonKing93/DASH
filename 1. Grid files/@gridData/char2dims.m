2cfee64d6f5f9ed93efde60b46166ce838e1a3aa
JonKing93 <jonking93@gmail.com>
2020-01-23 12:23:50 -0700

    ...

--- 1. Grid files/@gridData/char2dims.m
+++ 1. Grid files/@gridData/char2dims.m
@@ -1,16 +1,19 @@
 function[dims] = char2dims( dimChar )
 % Converts a comma delimited character array of dimension names to string
-comma = strfind(dimChar, ',');
-nDims = numel(comma) + 1;
-dims = cell(1, nDims);
+dims = "";
+if ~isempty(dimChar)
+    comma = strfind(dimChar, ',');
+    nDims = numel(comma) + 1;
+    dims = cell(1, nDims);
 
-start = [1, comma+1];
-stop = [comma-1, numel(dimChar)];
+    start = [1, comma+1];
+    stop = [comma-1, numel(dimChar)];
 
-for d = 1:nDims
-    dims{d} = dimChar( start(d):stop(d) );
+    for d = 1:nDims
+        dims{d} = dimChar( start(d):stop(d) );
+    end
+
+    dims = string( dims );
 end
-
-dims = string( dims );
 
 end
