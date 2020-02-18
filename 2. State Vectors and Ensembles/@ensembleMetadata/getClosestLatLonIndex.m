7ece4a6c54e2e1e6d68919acbe1c7b911e48e2e4
JonKing93 <jonking93@gmail.com>
2020-02-18 12:11:36 -0700

    Blocked no input warning message

--- 2. State Vectors and Ensembles/@ensembleMetadata/getClosestLatLonIndex.m
+++ 2. State Vectors and Ensembles/@ensembleMetadata/getClosestLatLonIndex.m
@@ -2,6 +2,17 @@ function[H] = getClosestLatLonIndex( obj, coords, varNames, varargin )
 % This function redirects to closestLatLonIndices. It is meant as a patch
 % to the rename to "closestLatLonIndices" of v3.4.0. It will be removed in
 % a future release.
-warning('ensembleMetadata.getClosestLatLonIndex was renamed and will be removed in a future release. You will eventually need to adjust your code to use ensembleMetadata.closestLatLonIndex instead.');
+warning(['ensembleMetadata.getClosestLatLonIndex has been renamed to ensembleMetadata.closestLatLonIndex and ',...
+        'will be removed in a future release. Please consider updating your code.']);
+
+if ~exist('coords','var')
+    coords = [];
+end
+if ~exist('varNames', 'var')
+    varNames = [];
+end
+if ~exist( 'varargin', 'var')
+    varargin = {};
+end
 H = obj.closestLatLonIndices( coords, varNames, varargin{:} );
 end
