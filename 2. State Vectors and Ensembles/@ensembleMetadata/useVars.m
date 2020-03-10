a24cf8adb0960827bbfd62ada2adb4c786614483
JonKing93 <jonking93@gmail.com>
2020-03-10 12:22:57 -0700

    meta.useVars

--- 2. State Vectors and Ensembles/@ensembleMetadata/useVars.m
+++ 2. State Vectors and Ensembles/@ensembleMetadata/useVars.m
@@ -1,30 +1,52 @@
 function[ensMeta] = useVars( obj, vars )
-% Returns ensemble metadata for a state vector limited to specified variables
+% Limits ensemble metadata to specific variables.
 % 
 % ensMeta = obj.useVars( vars )
 % Reduces the ensemble metadata to the specified variables.
 %
 % ----- Inputs -----
 % 
-% vars: A list of variable names. A string, cellstring, or character row
-%       vector.
+% vars: A logical vector indicating which variables are used.
 %
 % ----- Outputs -----
 %
 % ensMeta: The reduced ensemble metadata
 
+% Get the number of variables
+nVar = numel( obj.design.var );
+
 % Error check
-if ~isstrlist( vars )
-    error('vars must be a string vector, cellstring vector, or character row vector.');
+if ~isvector(vars) || ~islogical(vars) || numel(vars)~=nVar
+    error('vars must be a logical vector with %.f elements', nVar);
 end
-vars = string( vars );
 
-% Get the variables to keep and remove.
-keep = obj.varCheck( vars );
-remove = ~ismember( obj.varName, obj.varName(keep) );
+% Determine which variables to remove, and which to copy current metadata
+ensMeta = ensembleMetadata( obj.design );
+useVars = ensMeta.varName(vars);
+removeVars = ensMeta.varName( ~vars );
 
-% Reduce the metadata
-newDesign = obj.design.remove( obj.varName(remove) );
-ensMeta = ensembleMetadata( newDesign );
+remove = find( ~ismember(ensMeta.varName, useVars) );
+[copy, loc] = ismember(useVars, obj.varName);
+copyVars = useVars(copy);
+
+% Update the metadata
+ensMeta.varName(remove) = [];
+ensMeta.varSize(remove) = [];
+ensMeta.stateMeta = rmfield( ensMeta.stateMeta, removeVars );
+ensMeta.ensMeta = rmfield( ensMeta.ensMeta, removeVars );
+
+ensMeta.varSize(copy,:) = obj.varSize(loc,:);
+for v = 1:numel(copyVars)
+    ensMeta.stateMeta.(copyVars(v)) = obj.stateMeta.(copyVars(v));
+    ensMeta.ensMeta.(copyVars(v)) = obj.ensMeta.(copyVars(v));
+end
+
+% Recalculate limits and ensemble size
+ensMeta.varLimit(remove) = [];
+ensMeta.varLimit(copy,:) = obj.varLimit(copy,:);
+lastIndex = cumsum( ensMeta.varLimit(:,2) - ensMeta.varLimit(:,1) + 1 );
+firstIndex = [1; lastIndex(1:end-1)-1];
+ensMeta.varLimit = [firstIndex, lastIndex];
+ensMeta.ensSize(1) = lastIndex(end);
 
 end
