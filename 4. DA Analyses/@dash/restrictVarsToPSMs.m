979f1a3260ebb997b22ebceda9f49846bf79a98a
JonKing93 <jonking93@gmail.com>
2020-03-11 16:00:13 -0700

    Good build of restrictVarsToPSMs

--- 4. DA Analyses/@dash/restrictVarsToPSMs.m
+++ 4. DA Analyses/@dash/restrictVarsToPSMs.m
@@ -1,91 +1,80 @@
-function[F, M, ensMeta] = restrictVarsToPSMs( vars, F, M, ensMeta )
-%% Restricts specified variables to only the elements needed to run PSMs.
+function[F, varargout] = restrictVarsToPSMs( vars, F, meta, M )
 %
-% [F, M, ensMeta] = dash.restrictVarsToPSMs( vars, F, M, ensMeta )
-% Restricts specified variables to elements essential to PSMs. Returns the
-% reduced ensemble, associated (unstable) metadata, and PSMs with updated
-% state indices.
+% [F, ens] = restrictVarsToPSMs( vars, F, ens )
 %
-% ----- Inputs -----
+% [F, ensMeta] = restrictVarsToPSMs( vars, F, ensMeta )
 %
-% vars: A list of variable names. Cellstring, string, or character row
-% vector.
-%
-% F: A cell array of PSMs
-%
-% M: An ensemble. (nState x nEns)
-%
-% ensMeta: Metadata associated with the ensemble.
-%
-% ----- Outputs -----
-%
-% F: PSMs with updated state vector indices.
-%
-% M: The reduced ensemble.
-%
-% ensMeta: An unstable metadata object for the reduced ensemble. Can be
-%          used to regrid variables that were not restricted to PSM
-%          indices.
-%
-%          *** Warning: this ensemble metadata object should not be used
-%          for anything else. It is highly unstable...
+% [F, ensMeta, M] = restrictVarsToPSMs( vars, F, ensMeta, M )
 
-% Error checking
-if ~isvector(F) || ~iscell(F)
-    error('F must be a cell vector of PSM objects.');
-elseif ~isnumeric(M) || ~ismatrix(M)
-    error('M must be a numeric matrix');
-elseif ~isscalar(ensMeta) || ~isa(ensMeta, 'ensembleMetadata')
-    error('ensMeta must be a scalar ensembleMetadata object.');
-elseif ~isequal( size(M,1), ensMeta.ensSize(1) )
-    error('The number of rows in M does not match the size in the ensemble metadata.');
-end
-nState = size(M,1);
-
-% Check the variables exist. Get indices
-v = ensMeta.varCheck( vars );
-
-% Collect all the H indices. Error check individual PSMs
-nSite = numel(F);
-psmH = [];
-for s = 1:nSite
-    if ~isscalar(F{s}) || ~isa(F{s},'PSM')
-        error('Element %.f of F is not a scalar PSM object.',s);
-    elseif any(F{s}.H<1) || any(F{s}.H>nState) || any(mod(F{s}.H,1)~=0)
-        error('The state indices (H) for PSM %.f are not integers on the interval [1 %.f]', s, nState );
+% Parse inputs
+if isa(meta, 'ensemble')
+    if ~isscalar(meta)
+        error('ens must be a scalar ensemble object.');
+    else
+        ens = meta;
+        meta = ens.metadata;
     end
-    psmH = [psmH; F{s}.H]; %#ok<AGROW>
 end
 
-% Get the set of all H indices, and all indices associated with the
-% variables. Mark the variables as unregriddable.
-allH = (1:nState)';
-varH = [];
-for k = 1:numel(vars)
-    varH = [varH; ensMeta.varIndices( vars(k) )]; %#ok<AGROW>
+hasM = false;
+if exist('M','var')
+    hasM = true;
 end
 
-% Remove all indices for specified variables not in PSMs
-newH = allH( ~ismember(allH, varH) | ismember(allH, psmH) );
+% Error check
+if ~isscalar(meta) || ~isa(meta, 'ensembleMetadata')
+    error('meta must be a scalar ensembleMetadata object.');
+elseif ~isstrlist(vars)
+    error('vars must be a string vector, cellstring vector, or character row vector.');
+elseif ~isvector(F) || ~iscell(F)
+    error('F must be a cell vector.');
+elseif hasM && (~ismatrix(M) || ~isequal(size(M),meta.ensSize))
+    error('M must be a (%.f x %.f) matrix', meta.ensSize(1), meta.ensSize(2) );
+end
+v = unique( meta.varCheck(vars) );
+nVar = numel(v);
 
-% Update the ensemble and metadata
-M = M(newH, :);
-last = 0;
-for k = 1:numel(ensMeta.varName)
-    nIndex = sum( ismember(ensMeta.varIndices(ensMeta.varName(k)), newH) );
-    ensMeta.varLimit(k,:) = [last+1, last+nIndex];
-    if ismember(k,v)
-        ensMeta.noRegrid(k) = true;
+% Run through the PSMs, collect H indices
+nPSM = numel(f);
+indices = cell(nPSM, 1);
+for s = 1:nPSM
+    if ~isa(F{s}, 'PSM') || ~isscalar(F{s})
+        error('Element %.f of F is not a scalar PSM object.', s );
+    elseif isempty( F{s}.H )
+        warning('PSM %.f does not have state vector indices (H)', s );
     end
-    last = last+nIndex;
+    indices{s} = F{s}.H(:);
+end
+Hpsm = cell2mat(indices);
+
+% Get the set of all indices and restrictable indices
+indices = cell( nVar, 1 );
+for var = 1:nVar
+    indices{var} = obj.varIndices( obj.varName(v(var)) );
+end
+Hvar = cell2mat(indices);
+Hall = (1:obj.ensSize(1))';
+
+% Remove all indices not in PSMs
+Hnew = allH( ~ismember(Hall, Hvar) | ismember(Hall, Hpsm) );
+
+% Update the H indices in the PSMs
+for s = 1:nPSM
+    [~, psmH] = ismember( F{s}.H, newH );
+    F{s}.setStateIndices( psmH );
 end
 
-% Update the H indices in each PSM
-for s = 1:nSite
-    [~, newPsmH] = ismember( F{s}.H, newH );
-    F{s}.setStateIndices( newPsmH );
+% Update the ensemble / metadata / M
+if exist('ens','var')
+    ens.useStateIndices( Hnew );
+    varargout = {ens};
+else
+    meta = meta.useStateIndices( Hnew );
+    varargout = {meta};
+    if hasM
+        M = M(Hnew,:);
+        varargout = [varargout, {M}];
+    end
 end
 
 end
-    
-
