39f13fd9060c9af715640f0a65ba3784903cb1fc
JonKing93 <jonking93@gmail.com>
2020-03-11 14:29:20 -0700

    initial build of closestLatLonIndices for partial grid

--- 2. State Vectors and Ensembles/@ensembleMetadata/closestLatLonIndices.m
+++ 2. State Vectors and Ensembles/@ensembleMetadata/closestLatLonIndices.m
@@ -9,13 +9,13 @@ function[H] = closestLatLonIndices( obj, coords, varNames, varargin )
 % Searches each index associated with elements in the specified metadata.
 %
 % For Example:
-%   >> H = obj.closestLatLonIndices( ..., 'lev', [100 200 300] )
+%   >> H = obj.closestLatLonIndices( ..., 'lev', [100; 200; 300] )
 %
 %   would find three state elements. The closest on the level with metadata
 %   equal to 100, the closest on level 200, and the closest on level 300.
 %
 % Similarly:
-%   >> H = obj.closestLatLonIndices( ..., 'lev', [100 200], 'run', [1 2] )
+%   >> H = obj.closestLatLonIndices( ..., 'lev', [100; 200], 'run', [1; 2] )
 %
 %   would find 4 state elements. The closest on level 100 in runs 1 and 2,
 %   and the closest on level 200 in runs 1 and 2.
@@ -46,7 +46,6 @@ empty = repmat( {[]}, [nDim, 1] );
 [metaValue{:}] = parseInputs( varargin, cellstr(dimID), empty, empty );
 varNames = string(varNames);
 
-% Get the number of indices
 % Get the number of indices to search in each dimension. Throw error if
 % indices were provided for lat, lon, or tri
 nEls = zeros( nDim, 1 );
@@ -73,12 +72,11 @@ nVar = numel(varNames);
 H = NaN( nComb*nVar, nSite );
 
 % Get the v-index for each variable. Extract metadata for each sequence
-% element. Find the closest indices to the coordinates
+% element and find distances to coordinates
 for var = 1:numel(varNames)
     v = obj.varCheck( varNames(var) );
-    latlon = obj.getLatLonMetadata( varNames(var) );
+    latlon = obj.getLatLonSequence( varNames(var) );
     dist = haversine( coords, latlon );
-    [~, site] = min( dist, [], 2 );
     
     % Initialize the dimension search
     searchIndex = cell(nDim,1);
@@ -95,7 +93,7 @@ for var = 1:numel(varNames)
             error('The metadata search values do not have the same number of rows as the ensemble metadata.');
         end
         
-        [~, searchIndex{dim}] = ismember( metaValue{d}, obj.stateMeta.(obj.varName(v)).(dimID(d)), 'rows' );
+        [~, searchIndex{dim}] = ismember( metaValue{d}, stateMeta, 'rows' );
         if any( searchIndex{dim} == 0 )
             error('Variable %s does not have matching metadata along the %s dimension.', varNames(var), dimID(d) );
         end
@@ -103,14 +101,35 @@ for var = 1:numel(varNames)
         nModulus(dim) = prod( obj.varSize(v,1:d-1) );
     end
     
-    % Modulate the closest site (within a single sequence element), over
-    % the N-D set of search dimensions
-    nAdd = 0;
-    if ~isempty( subSearch )
-        nAdd = sum( (subSearch-1).*nModulus, 2 );
+    % On a complete grid, sequence lengths are preserved. Modulate the
+    % closest site over all search dimentions
+    if ~obj.partialGrid(v)
+        [~, site] = min( dist, [], 2 );
+        nAdd = 0;
+        if ~isempty( subSearch )
+            nAdd = sum( (subSearch-1).*nModulus, 2 );
+        end
+        H((var-1).*nComb+(1:nComb),:) = (obj.varLimit(v,1) - 1) + nAdd + site';
+        
+    % On a partial grid, cycle through the subsearches.
+    else
+        nEls = numel(dist);
+        for s = 1:nComb
+            
+            % Get the linear indices of the subgrid.
+            sub = ones(1, numel(dimID));
+            sub(searchDim) = subSearch(s,:);
+            sub = num2cell(sub);
+            ind = sub2ind( obj.varSize(v,:), sub{:}) + (0:nEls-1);
+            
+            % Use the partialH to reduce the distances. Find the minimum
+            % and get the H index
+            partial = obj.partialH{v}(ind);
+            seqdist = dist(:, partial);
+            [~, site] = min( seqdist, [], 2 );           
+            H((var-1).*nComb+s, :) = (obj.varLimit(v,1)-1) + sum(obj.partialH{v}(1:ind(1)-1)) + site;
+        end
     end
-    H((var-1).*nComb+(1:nComb),:) = (obj.varLimit(v,1) - 1) + nAdd + site';
-    
 end
 
 end
