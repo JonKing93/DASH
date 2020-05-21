dd464c4fee172b775002887e2192ab1356f0ea68
JonKing93 <jonking93@gmail.com>
2020-05-21 11:56:54 -0700

    Removed variance calculations from decompose

--- 4. DA Analyses/@dash/decompose.m
+++ 4. DA Analyses/@dash/decompose.m
@@ -1,8 +1,8 @@
-function[varargout] = decompose( X )
+function[Xmean, Xdev] = decompose( X )
 %% Break apart an ensemble into mean, deviations, and variance.
 %
-% [Xmean, Xdev, Xvar] = dash.decompose( X )
-% Gets the mean, deviation, and variance of an ensemble.
+% [Xmean, Xdev] = dash.decompose( X )
+% Gets the mean and deviations from the mean for an ensemble.
 %
 % ----- Inputs -----
 %
@@ -13,21 +13,8 @@ function[varargout] = decompose( X )
 % Xmean: Mean of the ensemble (nState x 1)
 %
 % Xdev: Ensemble deviations (nState x nEns)
-%
-% Xvar: Ensemble variance (nState x 1)
 
-% Mean
 Xmean = mean(X,2);
 Xdev = X - Xmean;
-
-% If only doing mean and deviation
-if nargout<3
-    varargout = {Xmean, Xdev};
-    
-% If also returning variance
-elseif nargout==3
-    Xvar = sum( Xdev.^2, 2 ) ./ (size(X,2)-1);
-    varargout = {Xmean, Xdev, Xvar};
-end
 
 end
