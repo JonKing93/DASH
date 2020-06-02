f9cd62218e6a4e30a1723307cbbca1e0669b91b9
JonKing93 <jonking93@gmail.com>
2020-06-02 13:57:02 -0700

    Precalculate sqrtm(Kdenom) to improve speed

--- 4. DA Analyses/jointOfflineKalmanFilter/kalmanAdjusted.m
+++ 4. DA Analyses/jointOfflineKalmanFilter/kalmanAdjusted.m
@@ -1,4 +1,5 @@
 function[Ka] = kalmanAdjusted( Knum, Kdenom, R )
 R = diag(R);
-Ka = Knum * (sqrtm(Kdenom)^(-1))' * (sqrtm(Kdenom) + sqrtm(R))^(-1);
+Ksqrt = sqrtm(Kdenom);
+Ka = Knum * (Ksqrt^(-1))' * (Ksqrt + sqrtm(R))^(-1);
 end
