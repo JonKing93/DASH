2e839d30442af9067fc04ca3f36549e13a8a98eb
JonKing93 <jonking93@gmail.com>
2020-06-25 10:03:58 -0700

    Rebuilding the dash class from scratch as a utilities class

---
+++ @dash/dash.m
@@ -0,0 +1,13 @@
+classdef dash
+    %% Contains various utility functions
+    
+    
+    methods (Static)
+        
+        % Global data for dimension names
+        names = dimensionNames;
+        
+    end
+    
+end
+        
