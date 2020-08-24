c66d8c07202e56c04979ae323dd658e4b61d177f
JonKing93 <jonking93@gmail.com>
2020-08-24 12:57:50 -0700

    doc tweak

--- @dash/assertStrFlag.m
+++ @dash/assertStrFlag.m
@@ -2,13 +2,13 @@ function[] = assertStrFlag( input, name )
 %% Checks that an input is a string flag. Returns a customized error
 % message if not.
 %
-% dash.assertStrFlags( input, name )
+% dash.assertStrFlag( input, name )
 %
 % ----- Inputs -----
 %
 % input: A variable being checked.
 %
-% names: The name of the variables to use in error messages. A string.
+% name: The name of the variable to use in the error message. A string.
 
 if ~dash.isstrflag(input)
     error('%s must be a string scalar or character row vector.',name);
