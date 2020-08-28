1a603d7378df0b4b775b94ac30aab78d871484bf
JonKing93 <jonking93@gmail.com>
2020-08-28 10:44:16 -0700

    Completed initial build of info console output

--- 2. State vectors/@stateVectorVariable/info.m
+++ 2. State vectors/@stateVectorVariable/info.m
@@ -19,7 +19,7 @@ fprintf('%s has %.f non-singleton dimensions: %s\n', obj.name, nDims, dash.messa
 % State dimensions
 sd = d(obj.isState(d));
 stateDims = obj.dims(sd);
-fprintf('\n\tSTATE DIMENSIONS: %s\n', dash.errorStringList(stateDims));
+fprintf('\n\tSTATE DIMENSIONS: %s\n', dash.messageList(stateDims));
 for k = 1:numel(stateDims)
     fprintf('\t%s has a length of %.f in the state vector.\n', stateDims(k), obj.size(sd(k)));
     
@@ -52,68 +52,41 @@ end
 % Ensemble dimensions
 sd = d(~obj.isState(d));
 ensDims = obj.dims(sd);
-fprintf('\n\tENSEMBLE DIMENSIONS: %s\n', dash.errorStringList(ensDims));
+fprintf('\n\tENSEMBLE DIMENSIONS: %s\n', dash.messageList(ensDims));
 for k = 1:numel(ensDims)
     fprintf('\t%s has %.f elements that can be used in an ensemble.\n', ensDims(k), numel(obj.ensIndices{sd(k)}));
     
     % Sequence information
     if ~isequal(obj.seqIndices{sd(k)}, 0)
-        strs = ["a sequence of data elements", "data indices"];
+        strs = ["a sequence of data elements:", "data indices"];
         if numel(obj.seqIndices{sd(k)}) == 1
             strs(1) = "the data element";
-        elseif isequal( abs(obj.seqIndices{sd(k)}), 1)
+        end
+        if isequal( abs(obj.seqIndices{sd(k)}), 1)
             strs(2) = "index";
         end
         list = dash.messageList(obj.seqIndices{sd(k)});
-        fprintf('\t\tIt has a length of %.f in the state vector, using %s %s %s after each reference element.\n', strs(1), list, strs(2));
+        fprintf('\t\tIt has a length of %.f in the state vector.\n', obj.size(sd(k)));
+        fprintf('\t\tIt uses %s %s %s after each reference element.\n', strs(1), list, strs(2));
     end
     
-    
-    
-    
-    
-    
-    
-    
+    % Mean information
+    if obj.takeMean(sd(k)) && ~isequal(obj.mean_Indices{sd(k)},0)
+        type = sprintf('\b');
+        if obj.hasWeights(sd(k)) && obj.meanSize(sd(k))>1
+            type = 'weighted';
+        end
+        strs = ["elements", "data indices"];
+        if numel(obj.mean_Indices{sd(k)}) == 1
+            strs(1) = "element";
+        end
+        if isequal(abs(obj.mean_Indices{sd(k)}),1)
+            strs(2) = "index";
+        end
+        list = dash.messageList(obj.mean_Indices{sd(k)});
+        fprintf('\t\tIt takes a %s mean over the data %s %s %s after each sequence element.\n', type, strs(1), list, strs(2));
+    end
 end
-%     
-%     
-%     
-%     
-%     a length of %.f in the state vector.', ensDims(k), obj.size(sd(k)));
-%     
-%     % String for mean
-%     if obj.size(sd(k))>1 || (obj.takeMean(sd(k)) && obj.meanSize(sd(k))>1)        
-%         if obj.meanSize(sd(k))==1 || ~obj.takeMean(sd(k))
-%             meanStr = sprintf('\b');
-%         elseif obj.hasWeights(sd(k)) 
-%             meanStr = sprintf('a weighted mean of %.f data elements', obj.meanSize(sd(k)));
-%         elseif obj.takeMean(sd(k))
-%             meanStr = sprintf('a mean of %.f data elements', obj.meanSize(sd(k)));
-%         end
-%         
-%         % String for spacing
-%         spaceStr = '';
-%         spacing = unique(diff(sort(obj.stateIndices{sd(k)})));
-%         if numel(spacing)==1 && spacing==1
-%             spaceStr = sprintf('spaced in steps of 1 data index');
-%         elseif numel(spacing)==1
-%             spaceStr = sprintf('spaced in steps of %.f data indices', spacing);
-%         end
-%         
-%         % 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
+
 fprintf('\n');
 end
