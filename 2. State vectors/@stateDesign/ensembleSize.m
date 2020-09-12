c2cf801c7d7072c02f56dcda08e6dd725f522f17
JonKing93 <jonking93@gmail.com>
2020-09-11 17:33:47 -0700

    Removed old stateDesign class

--- 2. State vectors/@stateDesign/ensembleSize.m
+++
@@ -1,7 +0,0 @@
-function[siz] = ensembleSize( obj )
-% Gets the size of the ensemble associated with the design
-nEns = size( obj.var(1).drawDex, 1 );
-varLimit = obj.varIndices;
-nState = varLimit(end);
-siz = [nState, nEns];
-end
