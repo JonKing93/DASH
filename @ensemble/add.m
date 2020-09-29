5e7534481e0b1b9d2d4835335819391870e8f64d
JonKing93 <jonking93@gmail.com>
2020-09-29 11:38:02 -0700

    Wrote minimal "ensemble.add" (still needs matfile error checks)

---
+++ @ensemble/add.m
@@ -0,0 +1,54 @@
+function[] = add(obj, nAdd, showprogress)
+%% Add additional state vectors to an ensemble saved in a .ens file.
+%
+% obj.add(nAdd)
+% Adds a specified number of state vectors to the ensemble.
+%
+% obj.add(nAdd, showprogress)
+% Specify whether to display a progress bar. Default is to show progress.
+%
+% ----- Inputs -----
+%
+% nAdd: The number of state vectors to add to the ensemble. A positive
+%    integer.
+%
+% showprogress: A scalar logical indicating whether to display a progress
+%    bar (true -- default), or not (false).
+
+% Default
+if ~exist('showprogress','var') || isempty(showprogress)
+    showprogress = true;
+end
+
+% Error check
+if ~isscalar(nAdd)
+    error('nAdd must be a scalar.');
+end
+dash.assertPositiveIntegers(nAdd, 'nAdd');
+dash.assertScalarLogical(showprogress, 'showprogress');
+
+% Get the matfile, check it is valid
+warning('Need error checking for file.');
+ens = matfile(obj.file,'Writable',true);
+
+% Record previous settings in case the operation fails
+sv = ens.stateVector;
+meta = ens.meta;
+
+% Pre-build the gridfiles and data sources. Check for validity
+[grids, sources, f] = sv.prebuildSources;
+
+% Add to the ensemble
+try
+    sv.buildEnsemble(nAdd, grids, sources, f, ens, showprogress);
+    
+% Remove failed portions of the file if the write operation fails
+catch
+    nCols = size(ens, 'X', 2); %#ok<GTARG>
+    ens.X(:,nPrevious+1:nCols) = [];
+    ens.hasnan(:,nPrevious+1:nCols) = [];
+    ens.meta = meta;
+    ens.stateVector = sv;
+end
+    
+end
--- @ensemble/ensemble.m
+++ @ensemble/ensemble.m
@@ -3,11 +3,59 @@ classdef ensemble
     
     properties
         file; % The .ens file associated with the 
+        
+        hasnan;
+        meta;
+        stateVector;
+        
         members; % Which ensemble members to load
         variables; % Which variables to load
     end
     
+    % Constructor
     methods
+        function obj = ensemble(filename)
+            %% Creates a new ensemble object
+            %
+            % obj = ensemble(filename)
+            % Finds a .ens file with the specified name on the active path
+            % and returns an associated ensemble object.
+            %
+            % obj = ensemble(fullname)
+            % Returns an ensemble object for a .ens file with the specified
+            % full file path.
+            %
+            % ----- Inputs -----
+            %
+            % filename: The name of a .ens file on the active path. A string.
+            %
+            % fullname: The full file path to a .ens file. A string.
+            %
+            % ----- Outputs -----
+            %
+            % obj: An ensemble object for the specified .ens file.
+            
+            % Check the input is a file name
+            file = dash.assertStrFlag(filename, "filename");
+            obj.file = dash.checkFileExists(file);
+            
+            warning('Constructor needs error checking');
+            m = matfile(obj.file);
+            obj.hasnan = m.hasnan;
+            obj.meta = m.meta;
+            obj.stateVector = m.stateVector;
+        end
+    end
+            
+            
+            
+            
+            
+            
+            
+    
+    methods
+        add(obj, nAdd, showprogress)
         load(obj);
         loadGrids(obj);
         loadMembers(obj, members);
