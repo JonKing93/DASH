1b03b751c93993bfc0c09d111b03c679f6c86102
JonKing93 <jonking93@gmail.com>
2020-10-02 12:52:54 -0700

    Added option flags for findRows

--- @ensembleMetadata/findRows.m
+++ @ensembleMetadata/findRows.m
@@ -2,13 +2,25 @@ function[rows] = findRows(obj, varName, varRows)
 %% Finds the state vector rows that correspond to elements of a specific
 % variable in a state vector.
 %
+% rows = obj.findRows(varName)
+% rows = obj.findRows(varName, 'all')
+% Returns the rows of all state vector elements associated with the
+% specified variable.
+%
+% rows = obj.findRows(varName, 'end')
+% Returns the last state vector row associated with a variable.
+%
 % rows = obj.findRows(varName, varRows)
+% Takes rows from a variable's state vector and locates the corresponding
+% rows in the complete state vector that contains all variables.
 %
 % ----- Inputs -----
 %
 % varName: The name of a variable in a state vector. A string.
 %
-% varRows: Elements of the state vector for the variable.
+% varRows: Elements of the variable's state vector. Either a set of linear
+%    indices or a logical vector with one element per state vector element
+%    for the variable.
 %
 % ----- Outputs -----
 %
@@ -18,11 +30,17 @@ function[rows] = findRows(obj, varName, varRows)
 varName = dash.assertStrFlag(varName, 'varName');
 v = dash.checkStrsInList(varName, obj.variableNames, 'varName', 'variable in the state vector');
 
-% Error check the indices
-name = sprintf('the number of state vector elements for variable "%s"', varName);
-varRows = dash.checkIndices(varRows, 'varRows', obj.nEls(v), name);
+% Parse secondary inputs
+if ~exist('varRows','var') || isempty(varRows) || isequal(varRows, 'all')
+    varRows = 1:obj.nEls(v);
+elseif isequal('varRows','end')
+    varRows = obj.nEls(v);
+else
+    name = sprintf('the number of state vector elements for variable "%s"', varName);
+    varRows = dash.checkIndices(varRows, 'varRows', obj.nEls(v), name);
+end
 
-% Get row in complete state vector.
+% Get rows in complete state vector.
 rows = varRows + obj.varLimit(v,1) - 1;
 
 end
