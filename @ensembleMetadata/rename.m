e3ea8600b0e932d7d653383c889681a791e6bb0e
JonKing93 <jonking93@gmail.com>
2020-10-16 16:48:07 -0700

    Added renamer to ensembleMetadata

---
+++ @ensembleMetadata/rename.m
@@ -0,0 +1,16 @@
+function[obj] = rename(obj, newName)
+%% Changes the identifying ensemble name for the ensembleMetadata object.
+%
+% obj = obj.rename(newName)
+%
+% ----- Inputs -----
+%
+% newName: The new identifying name for the ensemble. A string.
+%
+% ----- Outputs -----
+%
+% obj: The updated ensembleMetadata object.
+
+obj.name = dash.assertStrFlag(newName, 'newName');
+
+end
