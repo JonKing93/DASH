7e956f3490b3a2f5bf405183d29a9545f1724ae3
JonKing93 <jonking93@gmail.com>
2021-03-08 15:01:09 -0700

    Added R covariance to particle filter

--- @particleFilter/run.m
+++ @particleFilter/run.m
@@ -11,20 +11,37 @@ out.weights = NaN(pf.nSite, pf.nTime);
 if update
     out.A = NaN(pf.nState, pf.nTime);
 end
+if pf.Rcov
+    sse = NaN(pf.nEns, pf.nTime);
+end
 
 % Permute for singleton expansion
 D = permute(pf.D, [1 3 2]);
-R = permute(pf.R, [1 3 2]);
 
 % Process each prior, get the associated estimates and time steps
 for p = 1:pf.nPrior
     Y = pf.Y(:,:,p);
     t = pf.whichPrior==p;
-
-    % Get the sum of squared errors for each ensemble member. Use to
-    % compute particle filter weights
-    sse = nansum( (1./R).*(D - Y).^2, 1);
-    sse = squeeze(sse);
+    
+    % Vectorize sum of squared errors for R variance
+    innov = D(:,:,t) - Y;
+    if ~pf.Rcov
+        R = permute(pf.R(:,t), [1 3 2]);
+        sse = nansum( (1./R).*(innov).^2, 1);
+        sse = squeeze(sse);
+        
+    % Compute sum of squared errors in each time step for R covariance
+    else
+        for k = 1:numel(t)
+            time = t(k);
+            Rinv = pf.R(:,:,time)^-1;
+            for m = 1:pf.nEns
+                sse(m, time) = innov(:,m,k)' * Rinv * innov(:,m,k);
+            end
+        end
+    end
+    
+    % Compute particle filter weights
     w = pf.weights(sse);
     out.weights(:,t) = w;
     
