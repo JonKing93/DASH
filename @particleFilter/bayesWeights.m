7761b1bf790df633128b71eb974603cd322f00f8
JonKing93 <jonking93@gmail.com>
2021-05-12 14:33:37 -0700

    Added note on numerical stability in particleFilter.bayesWeights

--- @particleFilter/bayesWeights.m
+++ @particleFilter/bayesWeights.m
@@ -17,4 +17,18 @@ m = max(sse, [], 1);
 lse = m + log( sum( exp(sse-m), 1) );
 weights = exp(sse - lse);
 
+% Note on numerical stability:
+% 
+% Computing
+% Y = log( e^X1 + e^X2 + ... e^Xn )
+% is numerically unstable because e^(very positive) evaluates to Inf,
+% while e^(very negative) evaluates to 0.
+%
+% However, the alternative formulation:
+% Y = m + log( e^(X1-m) + e^(X2-m) + ... )
+% is more stable because the magnitude of the exponents is reduced.
+%
+% Also note that by choosing m as max(X), the largest exponent (X-m) is 0
+% and evaluated exactly.
+
 end
