5a6b8bdd9a252f8bed6f506adf04f60965e791e1
JonKing93 <jonking93@gmail.com>
2021-07-14 13:26:59 -0700

    Changed gridfile.load to use indices for 2 inputs. SCS requires both start and count

--- @gridfile/load.m
+++ @gridfile/load.m
@@ -1,4 +1,4 @@
-function[X, meta] = load(obj, dims, start, count, stride)
+function[X, meta] = load(obj, dims, indices, count, stride)
 %% Reads values from the data sources managed by a .grid file.
 %
 % [X, meta] = obj.load
@@ -12,10 +12,6 @@ function[X, meta] = load(obj, dims, start, count, stride)
 % [X, meta] = obj.load( dims, indices )
 % Only returns data at requested indices for specified dimensions. Loads
 % all elements of remaining dimensions.
-%
-% [X, meta] = obj.load(dims, start)
-% Specify the index at which to start loading data in specified dimensions.
-% Default is the first index.
 %
 % [X, meta] = obj.load(dims, start, count)
 % Specifies the number of elements to load from the specified dimensions.
@@ -45,9 +41,9 @@ function[X, meta] = load(obj, dims, start, count, stride)
 %
 % count: A vector whose elements indicate the number of elements to read 
 %    from specified dimensions. Must have one element for each dimension
-%    listed in dims and be in the same order as dims. Use Inf to read all
-%    elements from a dimension. If count = [], defaults to all elements in 
-%    each dimension.
+%    listed in dims and be in the same order as dims. Use Inf to read until
+%    the end of a dimension. If count = [], defaults to Inf for all
+%    dimensions.
 %
 % stride: A vector whose elements indicate the inter-element spacing to use
 %    when loading data from specified dimensions. Must have one element for
@@ -67,87 +63,82 @@ function[X, meta] = load(obj, dims, start, count, stride)
 % Update the object in case the file changed
 obj.update;
 
-% Defaults and error check the dimensions
-if ~exist('dims','var') || isempty(dims)
-    dims = obj.dims(obj.isdefined);
-end
-dims = dash.assert.strlist(dims, "dims");
-obj.checkAllowedDims(dims, false);
-if numel(dims) < numel(unique(dims))
-    error('dims contains duplicate names.');
-end
-nInputDims = numel(dims);
-
-% Defaults for start count stride
-if ~exist('start','var') || isempty(start)
-    start = ones(1, nInputDims);
-end
-if ~exist('count','var') || isempty(count)
-    count = Inf(1, nInputDims);
-end
-if ~exist('stride','var') || isempty(stride)
-    stride = ones(1, nInputDims);
-end
-
-% Parse the input style: indices vs start,count,stride
-haveIndices = false;
-if iscell(start)
-    haveIndices = true;
-    inputIndices = start;
-    if nargin>3
-        error('There can only be two input arguments (dims and indices) when specifying indices.');
+% Default and error check dimensions
+if ~exist('dims','var')
+    inputOrder = [];
+else
+    dims = dash.assert.strlist(dims, "dims");
+    obj.checkAllowedDims(dims, false);
+    if numel(dims) < numel(unique(dims))
+        error('dims contains duplicate names');
     end
+    
+    % Count input dimensions and map to internal grid dimensions
+    nInputDims = numel(dims);
+    [~, inputOrder] = ismember(dims, obj.dims);
 end
 
-% Map dims to the internal grid dimensions
-[~, inputOrder] = ismember(dims, obj.dims);
-
-% If indices were not specified, generate them from start, count, stride
-if ~haveIndices
-    inputIndices = cell(1, nInputDims);
-
+% Default indices
+if ~exist('indices','var')
+    indices = [];
+    
+% If indices are provided directly, parse and error check the cell vector
+elseif exist('indices','var') && ~exist('count','var')
+    [indices, wasCell] = dash.parse.inputOrCell(indices, nInputDims, "indices");
+    name = 'indices';
+    
+    % Error check indices for individual dimensions
+    for d = 1:nInputDims
+        if wasCell
+            name = sprintf('Element %.f of indices', d);
+        end
+        lengthName = sprintf('the length of the %s dimension', obj.dims(inputOrder(d)));
+        indices{d} = dash.assert.indices(indices{d}, name, obj.size(inputOrder(d)), lengthName );
+    end
+    
+% Otherwise use start/count/stride syntax. Get defaults
+else
+    start = indices;
+    if isempty(start)
+        start = ones(1, nInputDims);
+    end
+    if isempty(count)
+        count = Inf(1, nInputDims);
+    end
+    if ~exist('stride','var') || isempty(stride)
+        stride = ones(1, nInputDims);
+    end
+    
     % Error check start, count, stride inputs
-    input = {start, count, stride};
+    scs = {start, count, stride};
     name = ["start","count","stride"];
-    allowInf = [false true false];
-    for i = 1:numel(input)
-        dash.assert.vectorTypeN( input{i}, 'numeric', nInputDims, name(i) );
-        dash.assert.positiveIntegers( input{i}, name(i), false, allowInf(i) );
-        if any( input{i}>obj.size(inputOrder) & ~isinf(input{i}) )
-            bad = find(input{i}>obj.size(inputOrder),1);
+    allowInf = [false, true, false];
+    for i = 1:3
+        dash.assert.vectorTypeN( scs{i}, 'numeric', nInputDims, name(i) );
+        dash.assert.positiveIntegers( scs{i}, name(i), false, allowInf(i) );
+        if any( scs{i}>obj.size(inputOrder) & ~isinf(scs{i}) )
+            bad = find(scs{i}>obj.size(inputOrder),1);
             error('Element %.f of %s (%.f) is larger than the length of the %s dimension (%.f)', bad, name(i), start(bad), obj.dims(inputOrder(bad)), obj.size(inputOrder(bad)) );
         end
     end
-
-    % Get the indices. Check they don't exceed the length of the dimension.
-    count(isinf(count)) = obj.size(inputOrder(isinf(count)));
+    
+    % Get the count for Inf elements
+    k = isinf(count);
+    count(k) = ceil(  (obj.size(inputOrder(k))-start(k)+1) / stride(k)  );
+    
+    % Get the indices. Check they don't exceed the dimension length
+    indices = cell(1, nInputDims);
     for d = 1:nInputDims
         stop = start(d) + stride(d)*(count(d)-1);
         if stop > obj.size(inputOrder(d))
-            error('The start, count, and stride for dimension %s specify values up to %.f, which is larger than the length of the dimension %.f', obj.dims(inputOrder(d)), stop, obj.size(inputOrder(d)) );
+            error('The start, count, and stride for dimension %s specify values up to %.f, which is larger than the length of the dimension (%.f)', ...
+                obj.dims(inputOrder(d)), stop, obj.size(inputOrder(d)) );
         end
-        inputIndices{d} = start(d):stride(d):stop;
-    end
-
-% User specified indices. Error check cell
-else
-    dash.assert.vectorTypeN(inputIndices, 'cell', nInputDims, 'indices');
-    
-    % Default for empty indices
-    for d = 1:nInputDims
-        if isempty(inputIndices{d})
-            inputIndices{d} = 1:obj.size(inputOrder(d));
-        end
-        
-        % Error check indices for individual dimensions
-        name = sprintf('Element %.f of indices', d);
-        lengthName = sprintf('the length of the %s dimension', obj.dims(inputOrder(d)));
-        inputIndices{d} = dash.assert.indices(inputIndices{d}, name, obj.size(inputOrder(d)), lengthName );
+        indices{d} = start(d):stride(d):stop;
     end
 end
 
-% Load the values. The "load" method doesn't actually make repeated load
-% operations, so will use a placeholder dataSource array.
-[X, meta] = obj.repeatedLoad(inputOrder, inputIndices);
+% Load the values
+[X, meta] = obj.repeatedLoad(inputOrder, indices);
 
 end
