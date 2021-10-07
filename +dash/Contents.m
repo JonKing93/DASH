844174411c068c60006cf827961160e220b9e13e
JonKing93 <jonking93@gmail.com>
2021-10-07 09:17:53 -0700

    Added Contents.m to +dash

---
+++ +dash/Contents.m
@@ -0,0 +1,11 @@
+% Contents of the <strong>dash</strong> package:
+%
+% Functions
+%   displayContents - Prints a formatted description of package contents to the console
+%   
+% Utility Subpackages
+%   assert - Methods that throw errors when an input does not match required criteria
+%       is - Methods that test whether an input is a particular type
+%
+% Tests
+%   tests - Implements unit tests for functions, classes, and sub-packages
