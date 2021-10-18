028b537cb4eb77e6a58a23139a60d6cfe5f8e1f5
JonKing93 <jonking93@gmail.com>
2021-10-18 15:32:59 -0700

    arithmetic doc

--- +gridarithmetic/arithmetic.m
+++ +gridarithmetic/arithmetic.m
@@ -3,9 +3,58 @@ function[] = arithmetic(obj, operation, grid2, filename, overwrite, attributes, 
 % ----------
 %   obj.arithmetic(operation, grid2, filename, overwrite, atts, type)
 %   Implement an arithmetic operation on the data in two .grid files. Saves
-%   the result of the operation to a .mat file and catalogue the result in
+%   the result of the operation to a .mat file and catalogues the result in
 %   a new .grid file.
 % ----------
+%   Inputs:
+%       operation ('plus' | 'minus' | 'times' | 'divide'): The arithmetic
+%           operation to apply to the two gridfiles
+%       grid2 (string scalar | gridfile object): The second gridfile to use in
+%           the arithmetic operation.
+%       filename (string scalar | string vector[2]): The name to use for the
+%           new .mat and .grid files. If a single file name, the name will be
+%           used for both files. If two file names, the first name is used for
+%           the .mat file, and the second name is used for the .grid file.
+%       overwrite (scalar logical | logical vector[2]): Whether to overwrite
+%           existing .mat and .grid files. True allows overwriting, false does
+%           not. If scalar, uses the same option for both files. If two
+%           elements, uses the first option for the .mat file, and the second
+%           option for the .grid file. Default is false.
+%       attributes (1 | 2 | scalar struct | empty array): Options for
+%           setting metadata attributes in the new .grid file. If 1, copies
+%           the attributes from the first gridfile to the new file. If 2,
+%           copies the attributes from grid2 to the new file. If a scalar
+%           struct, uses the struct directly as the attributes. If unset or
+%           an empty array, the new file will have no metadata attributes.
+%       type (1 | 2 | 3): Options for matching gridfile metadata and sizes.
+%           [1 (default)]: requires data dimensions to have compatible sizes
+%           AND have the same metadata along each non-singleton dimension.
+%           Does arithmetic on all data elements.
+%           [2]: Searches for data elements with matching elements in
+%           non-singleton dimensions. Only does arithmetic at these elements.
+%           Does not require data dimensions to have compatible sizes.
+%           [3]: Does not compare dimensional metadata. Loads all data elements
+%           from both files and applies arithmetic directly. Requires data
+%           dimensions to have compatible sizes.
+%
+%   Outputs:
+%       Creates a .mat and .grid file with the specified names
+%
+%   Throws:
+%       DASH:gridfile:<operation>:invalidGridfile  when grid2 is not a
+%           valid gridfile
+%       DASH:gridfile:<operation>:invalidAttributes  when attributes is not
+%           a recognized option
+%       DASH:gridfile:<operation>:invalidType  when type is not a
+%           recognized option
+%       DASH:gridfile:<operation>:dimensionLengthMismatch  when type is 1
+%           or 3 and the data dimensions do not have compatible sizes
+%       DASH:gridfile:<operation>:differentMetadata  when type is 1 and
+%           there is different metadata along a non-singleton dimension
+%       DASH:gridfile:<operation>:noMatchingMetadata  when type is 2 and
+%           there is no matching metadata along a non-singleton dimension
+%
+% <a href="matlab:dash.doc('gridfile.arithmetic')">Online Documentation</a>
 
 %% Operation specific details
 
@@ -18,7 +67,7 @@ if strcmpi(operation, 'plus')
     strs = ["sum", "with"];
 elseif strcmpi(operation, 'minus')
     math = @minus;
-    strs = ["subrtract","from"];
+    strs = ["subtract","from"];
 elseif strcmpi(operation, 'times')
     math = @times;
     strs = ["multiply","by"];
@@ -125,7 +174,7 @@ for d = 1:numel(xdims)
             [meta.(dim), order1{d1(d)}, order2{d2(d)}] = ...
             intersect(meta1.(dim), meta2.(dim), 'rows', 'stable');
             if isempty(meta.(dim))
-                noOverlapError(strs, obj.name, grid2.name, dim, header);
+                noMatchingMetadataError(strs, obj.name, grid2.name, dim, header);
             end
         else
             if type==1
@@ -205,8 +254,8 @@ error(id, ['Cannot %s %s %s %s because the files have different metadata along '
     'the %s dimension. (Element %.f in %2$s is not in %1$s)'],...
     strs(1), name1, strs(2), name2, dim, index);
 end
-function[] = noOverlapError(strs, name1, name2, dim, header)
-id = sprintf('%s:noOverlappingMetadata', header);
-error(id, ['Cannot %s %s %s %s because there is no overlapping metadata along ',...
+function[] = noMatchingMetadataError(strs, name1, name2, dim, header)
+id = sprintf('%s:noMatchingMetadata', header);
+error(id, ['Cannot %s %s %s %s because there is no matching metadata along ',...
     'the %s dimension'], strs(1), name1, strs(2), name2, dim);
 end
