75f94917faa3bfd847411c753ad4496c65b71bc4
JonKing93 <jonking93@gmail.com>
2021-10-26 10:41:50 -0700

    Adjusted formatting of dash.assert.Contents
    Template Contents.m file

--- +dash/+assert/Contents.m
+++ +dash/+assert/Contents.m
@@ -1,30 +1,27 @@
-%% dash.assert
-% Functions that assert inputs meet required conditions
+%% dash.assert  Functions that assert inputs meet required conditions
+% ----------
+%   The dash.assert subpackage facilitates the creation of informative
+%   error messages and helps provide consistent error ID handling for common
+%   input error checks. These functions produce error messages that appear to
+%   originate from higher level calling functions.
 %
-% The dash.assert subpackage facilitates the creation of informative
-% error messages and helps provide consistent error ID handling for common
-% input error checks. These functions produce error messages that appear to
-% originate from higher level calling functions.
-%
-% Some functions also return inputs converted to specific formats to ensure
-% consistent internal handling of different input types.
-%
+%   Some functions also return inputs converted to specific formats to ensure
+%   consistent internal handling of different input types.
+% ----------
 % Strings:
-%       strflag - Throw error if input is not a string flag.
-%       strlist - Throw error if input is not a string vector, cellstring vector, or char row vector
-%    strsInList - Throw error if strings are not in a list of allowed strings
-%
-% Files:
-%    fileExists - Throw error if a file does not exist
+%   strflag     - Throw error if input is not a string flag.
+%   strlist     - Throw error if input is not a string vector, cellstring vector, or char row vector
+%   strsInList  - Throw error if strings are not in a list of allowed strings
 %
 % Size and Data Type:
-%          type - Throw error if input is not required type
-%    scalarType - Throw error if input is not a scalar of a required data type
+%   type        - Throw error if input is not required type
+%   scalarType  - Throw error if input is not a scalar of a required data type
 %   vectorTypeN - Throw error if input is not a vector of required data type and length
 %
-% Numeric:
+% Files:
+%   fileExists  - Throw error if a file does not exist
 %
 % Tests:
-%         tests - Implement unit testing for the dash.assert subpackage
+%   tests       - Implement unit testing for the dash.assert subpackage
 %
-% <a href="matlab:dash.doc('dash.assert')">Online Documentation</a>
+% <a href="matlab:dash.doc('dash.assert')">Documentation Page</a>
