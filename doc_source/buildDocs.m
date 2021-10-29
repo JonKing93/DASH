17fce1b0f5a31aff8f7b4c8df7ef9181e6a80791
JonKing93 <jonking93@gmail.com>
2021-10-29 11:02:32 -0700

    Created buildDocs for automated build regardless of install location
    Should work for toolboxes, other computers, etc
    
    *Note*: Will still need to adjust python packages using mods in doc_source/sphinx/python-package-mods

--- doc_source/buildDocs.m
+++ doc_source/buildDocs.m
@@ -1,8 +1,45 @@
 function[] = buildDocs
+%% Builds the DASH HTML documentation pages
 
-source = 'C:\Users\jonki\Documents\MATLAB\DASH\doc_source\sphinx-source-rst';
-build = 'C:\Users\jonki\Documents\MATLAB\DASH\doc_source\html';
-command = sprintf('sphinx-build -a %s %s', source, build);
+% Get the location of the doc builder
+fullpath = mfilename('fullpath');
+folders = split(fullpath, filesep);
+folders = folders(1:end-1);
+docSource = strjoin(folders, filesep);
+
+% Move to the doc builder, but return to current location when finished
+currentDir = pwd;
+goback = onCleanup( @()cd(currentDir) );
+cd(docSource);
+
+% Clear the source .rst folder
+source = fullfile(docSource, 'sphinx', 'source');
+if isfolder(source)
+    rmdir(source, 's');
+end
+mkdir(source);
+
+% Move to the source folder and build .rst files
+cd(source);
+dashRoot = fullfile(folders{1:end-1});
+examplesRoot = fullfile(docSource, 'examples');
+doc.dash(dashRoot, examplesRoot);
+
+% Add the sphinx config and index rst to the sphinx reST source
+conf = fullfile(docSource, 'sphinx', 'config', 'conf.py');
+copyfile(conf, source);
+index = fullfile(docSource, 'sphinx', 'config', 'index.rst');
+copyfile(index, source);
+
+% Clear the HTML build
+build = fullfile(docSource, 'build', 'html');
+if isfolder(build)
+    rmdir(build, 's');
+end
+mkdir(build);
+
+% Use sphinx to build the html pages
+command = sprintf('sphinx-build -qa %s %s', source, build);
 system(command);
 
 end
