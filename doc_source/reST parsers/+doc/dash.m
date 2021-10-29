1f646e0f787bc817e4c48609f2eef9b9660e1613
JonKing93 <jonking93@gmail.com>
2021-10-29 11:00:52 -0700

    Created doc builder for toolbox
    Distinct from package because toolbox is not on the title chain

---
+++ doc_source/reST parsers/+doc/dash.m
@@ -0,0 +1,21 @@
+function[] = dash(root, examplesRoot)
+%% Builds the documentation for all of DASH
+% 
+% Different from doc.package because the toolbox does not have a title to
+% add to the title chain
+
+% Get the docs to include
+include = ["+dash"];
+
+% Get the contents
+contents = dir(root);
+files = string({contents.name});
+files = files(ismember(files, include));
+contents = strcat(root, filesep, files);
+
+% Document the contents
+for c = 1:numel(contents)
+    doc.content("", contents(c), examplesRoot);
+end
+
+end
