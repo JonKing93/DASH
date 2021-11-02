149e1513f1dee2e04beb5a06a00f5cb0fcc21724
JonKing93 <jonking93@gmail.com>
2021-11-02 13:47:09 -0700

    Added hidden constructor to initialize empty gridfile
    can only be called from gridfile.new

--- @gridfile/new.m
+++ @gridfile/new.m
@@ -1,12 +1,12 @@
-function[grid] = new(filename, metadata, overwrite)
+function[obj] = new(filename, metadata, overwrite)
 %% gridfile.new  Create a new (empty) .grid file
 % ----------
-%   grid = gridfile.new(filename, metadata)
+%   obj = gridfile.new(filename, metadata)
 %   Initializes a new (empty) .grid file. The new file manages an
 %   N-dimensional grid that spans the provided metadata. Adds a ".grid"
 %   extension to the filename if it does not already have one.
 %
-%   grid = gridfile.new(filename, metadata, overwrite)
+%   obj = gridfile.new(filename, metadata, overwrite)
 %   Specify whether the method may overwrite pre-existing .grid files. By
 %   default, the method will not overwrite files.
 % ----------
@@ -19,7 +19,7 @@ function[grid] = new(filename, metadata, overwrite)
 %           (true) or not (false). Default is false.
 %
 %   Outputs:
-%       grid (gridfile object): A gridfile object for the new .grid file.
+%       obj (gridfile object): A gridfile object for the new .grid file.
 
 % Default
 if ~exist('overwrite','var') || isempty(overwrite)
@@ -35,41 +35,25 @@ dash.assert.scalarType(metadata, 'gridMetadata', 'meta', header);
 dash.assert.scalarType(overwrite, 'logical', 'overwrite', header);
 filename = dash.file.new(filename, ".grid", overwrite, header);
 
-% Get the dimensions and sizes
-dims = metadata.defined;
-nDims = numel(dims);
-sizes = NaN(1, nDims);
+% Initialize empty gridfile object
+obj = gridfile;
+
+% Set the metadata, dimensions, initialize dimLimit
+obj.meta = metadata;
+obj.dims = metadata.defined';
+nDims = numel(obj.dims);
+obj.dimLimit = NaN(nDims, 2, 0);
+
+% Get dimension sizes
+obj.sizes = NaN(1, nDims);
 for d = 1:nDims
-    sizes(d) = size(metadata.(dims(d)), 1);
+    dim = obj.dims(d);
+    obj.sizes(d) = size(metadata.(dim), 1);
 end
 
-% Initialize deafult transformations
-fill = NaN(0,1);
-range = NaN(0,1);
-transform = false;
-transform_type = [];
-transform_params = [];
-
-% Initialize data sources
-source = [];
-relativePath = [];
-dimLimit = [];
-source_fill = [];
-source_range = [];
-source_transform = [];
-source_transform_type = [];
-source_transform_params = [];
-
-% Save
-save(filename, '-mat', ...
-    'metadata', 'dims', 'sizes',...
-    'fill','range','transform','transform_type','transform_params',...
-    'source', 'relativePath', 'dimLimit',...
-    'source_fill','source_range',...
-    'source_transform','source_transform_type','source_transform_params'...
-    );
-
-% Get object
-grid = gridfile(filename);
+% Save the file. Update file name to absolute path
+obj.file = filename;
+obj.save;
+obj.file = dash.assert.fileExists(obj.file);
 
 end
