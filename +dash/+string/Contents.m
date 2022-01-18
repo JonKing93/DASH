e5bdf2a13b4e7852aea707d76a182fecb6b02e8b
JonKing93 <jonking93@gmail.com>
2022-01-18 12:56:35 -0700

    Added string.elementName

--- +dash/+string/Contents.m
+++ +dash/+string/Contents.m
@@ -1,7 +1,8 @@
 %% dash.string  Functions that manipulate strings
 % ----------
 % Functions:
-%   list  - List the elements of a vector in a formatted sting
+%   list        - List the elements of a vector in a formatted sting
+%   elementName - Return a name for an element in a list
 %
 % Unit Tests:
 %   tests - Unit tests for the dash.string package
