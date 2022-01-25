bc785866564cabeecd157dcc2eb8900f987d549f
JonKing93 <jonking93@gmail.com>
2022-01-25 16:31:20 -0700

    strip verbosity from sv.couple

--- @stateVector/couple.m
+++ @stateVector/couple.m
@@ -1,4 +1,4 @@
-function[obj] = couple(obj, variables, verbose)
+function[obj] = couple(obj, variables)
 %% stateVector.couple  Match variable metadata when building ensemble members
 % ----------
 %   obj = obj.couple(v)
@@ -16,15 +16,7 @@ function[obj] = couple(obj, variables, verbose)
 %   updated.
 %
 %   Coupling is transitive, so unlisted variables that are coupled to one
-%   of the listed variables will also be coupled. If you have enabled the
-%   verbose option for the state vector, then this method will print a
-%   list of these secondary coupled variables to the console. (See the
-%   "stateVector.verbose" method to adjust state vector verbosity).
-%
-%   obj = obj.couple(variables, verbose)
-%   Specify whether the method should print the list of secondary coupled
-%   variables to the console. This syntax allows you to override the state
-%   vector's default verbosity setting. 
+%   of the listed variables will also be coupled.
 % ----------
 %   Inputs:
 %       v (logical vector | linear indices): The indices of variables in
@@ -33,10 +25,6 @@ function[obj] = couple(obj, variables, verbose)
 %           of linear indices.
 %       variableNames (string vector): The names of variables
 %           in the state vector that should be coupled.
-%       verbose (scalar logical | string scalar): Indicates whether the method
-%           should print a list of secondary coupled variables to the console.
-%           [true | "v" | "verbose"]: Print the list
-%           [false | "q" | "quiet"]: Do not print the list
 %
 %   Outputs:
 %       obj (scalar stateVector object): The state vector with updated
@@ -63,15 +51,6 @@ header = "DASH:stateVector:couple";
 dash.assert.scalarObj(obj, header);
 obj.assertEditable;
 
-% Default, error check verbosity
-if ~exist('verbose','var') || isempty(verbose)
-    verbose = obj.verbose;
-else
-    offOn = {["q","quiet"], ["v","verbose"]};
-    verbose = dash.parse.switches(verbose, offOn, 1, ...
-        'verbose', 'recognized verbosity setting', header);
-end
-
 % Check user variables, get indices
 uv = obj.variableIndices(variables, true, header);
 
@@ -82,12 +61,6 @@ av = unique(col);
 % Update coupled variables to match the template
 tv = uv(1);
 obj = obj.updateCoupledVariables(tv, av);
-
-% Notify user of secondary coupling
-if verbose
-    sv = av(~ismember(av, uv));
-    notifySecondaryCoupling(sv);
-end
 
 % Couple the variables
 for k = 1:numel(av)
