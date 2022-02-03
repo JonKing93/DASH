c31d3911043a5a9d990860819f45f077a614f48b
JonKing93 <jonking93@gmail.com>
2022-02-03 15:59:37 -0700

    restructure index limits to accept all or ens dimensions

--- +dash/@stateVectorVariable/indexLimits.m
+++ +dash/@stateVectorVariable/indexLimits.m
@@ -1,21 +1,39 @@
-function[limits] = indexLimits(obj, dims, subMembers)
+function[limits] = indexLimits(obj, dims, subMembers, includeState)
 %% dash.stateVectorVariable.indexLimits  Return the gridfile index limits required to load a set of ensemble members
 % ----------
-%   limits = obj.indexLimits(dims, subMembers)
+%   limits = obj.indexLimits(dims, subMembers, includeState)
+%   Returns index limits along dimensions of a variable. Size of the output
+%   and dimension order of limits will depend on whether state dimension
+%   limits are required or not.
+%
+%   allLimits = obj.indexLimits(dims, subMembers, true)
 %   Determines the gridfile indices needed to load all the indicated
-%   ensemble members. Returns the limits of these indices along each
-%   dimension.
+%   ensemble members. Includes limits for all dimensions in the variable.
+%   The order of limits is the order of dimensions in the variable.
+%
+%   ensLimits = obj.indexLimits(dims, subMembers, false)
+%   Determines the index limits along ensemble dimensions required to load
+%   the indicated ensemble members. Only includes limits for ensemble
+%   dimensions. The order of limits is the order of columns of subMembers
+%   (the ensemble dimension order for the coupling set).
 % ----------
 %   Inputs:
 %       dims (vector, linear indices [nEnsDims]): The dimension indices that
 %           correpsond to the columns of subMembers.
-%       subMembers (matrix, linear indices [nInitial x nEnsDims]): A set of
+%       subMembers (matrix, linear indices [nMembers x nEnsDims]): A set of
 %           dimensionally-subscripted ensemble members for the variable.
+%       includeState (scalar logical): Set to true if the method should
+%           determine the limits of state dimensions. Set to false to only
+%           determine limits of ensemble dimensions.
 %
 %   Outputs:
-%       limits (matrix, linear indices [nDims x 2]): The limits of the
+%       allLimits (matrix, linear indices [nDims x 2]): The limits of the
 %           indices required to load the ensemble members along each
-%           gridfile dimension.
+%           ensemble dimension. The rows of limits are in the same
+%           dimension order as the dimensions of the variable
+%       ensLimits (matrix, linear indices [nEnsDims x 2]): The limits along
+%           the ensemble dimensions of the coupling set. Limits are in the
+%           same order as the columns of subMembers.
 %
 % <a href="matlab:dash.doc('dash.stateVectorVariable.indexLimits')">Documentation Page</a>
 
@@ -23,40 +41,38 @@ function[limits] = indexLimits(obj, dims, subMembers)
 nDims = numel(obj.dims);
 limits = NaN(nDims, 2);
 
-% Cycle through dimensions. Get indices
+% Cycle through dimensions. Optionally ignore state dimensions
 for d = 1:nDims
-    indices = obj.indices{d};
-    if isempty(indices)
-        indices = 1:obj.gridSize(d);
+    if obj.isState(d) && ~includeState
+        continue
     end
 
-    % For state dimensions, get direct index limits
+    % For state dimensions, get index limits directly
     if obj.isState(d)
-        minIndex = min(indices);
-        maxIndex = max(indices);
+        limits(d,1) = min(obj.indices{d});
+        limits(d,2) = max(obj.indices{d});
 
-    % For ensemble dimensions, get the indices for the ensemble members
+    % For ensemble dimensions, get min/max reference indices
     else
-        k = d==dims;
-        indices = indices(subMembers(:,k));
-
-        % Then extract limits
+        k = d == dims;
+        indices = obj.indices{d}(subMembers(:,k));
         minIndex = min(indices);
         maxIndex = max(indices);
-
+    
         % Get minimum and maximum add indices
         addIndices = obj.addIndices(d);
         maxSubtract = min(addIndices);
         maxAdd = max(addIndices);
-
+    
         % Adjust index limits for add indices
-        minIndex = minIndex + maxSubtract;
-        maxIndex = maxIndex + maxAdd;
+        limits(d,1) = minIndex + maxSubtract;
+        limits(d,2) = maxIndex + maxAdd;
     end
+end
 
-    % Record the final limits
-    limits(d,1) = minIndex;
-    limits(d,2) = maxIndex;
+% Reorder if ensemble only
+if ~includeState
+    limits = limits(dims,:);
 end
 
 end
