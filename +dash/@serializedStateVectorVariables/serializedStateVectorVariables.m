638ec1068004930f16b0c8b570fee4325214988e
JonKing93 <jonking93@gmail.com>
2022-02-18 11:43:01 -0700

    Add docs and empty constructor to serialized svv

--- +dash/@serializedStateVectorVariables/serializedStateVectorVariables.m
+++ +dash/@serializedStateVectorVariables/serializedStateVectorVariables.m
@@ -1,5 +1,38 @@
 classdef serializedStateVectorVariables
-    %% dash.serializedStateVectorVariables  Serialize a vector of stateVectorVariable objects to support fast save/load operations.
+    %% dash.serializedStateVectorVariables  Serialize vector of stateVectorVariable objects to support fast save/load operations.
+    % ----------
+    %   The dash.serializedStateVectorVariables class implements an object
+    %   that can serialize a vector of dash.stateVectorVariable objects.
+    %
+    %   stateVectorVariable objects are highly nested. This helps to
+    %   organize design parameters, and also make associated code more
+    %   readable. However, a consequence of this nested layout is that
+    %   stateVectorVariable objects are slow to save and load from files.
+    %   This issue is most noticeable for large vectors of
+    %   stateVectorVariable objects. 
+    % 
+    %   The serializedStateVectorVariables class is designed to address
+    %   this issue. The class converts the information stored in a vector
+    %   of stateVectorVariable objects into a format that supports fast
+    %   save/load operations. Although the serialized object can no longer be used
+    %   for stateVectorVariable operations, the original vector can be
+    %   rebuilt using the "deserialize" method. Additionally, the
+    %   properties of the serialized object can only be set privately. This
+    %   ensures that serialized information is read-only, thereby
+    %   removing the need to error check the validity of deserialized
+    %   vectors.
+    % ----------
+    % dash.serializedStateVectorVariables methods:
+    %
+    % Serialization:
+    %   serializedStateVectorVariables  - Creates a serialized object from a vector of stateVectorVariable objects
+    %   serialize                       - Serializes a vector of stateVectorVariable objects
+    %   deserialize                     - Rebuilds a vector of stateVectorVariable objects from a serialized object
+    %
+    % Unit tests:
+    %   tests                           - Implement unit tests for the serializedStateVectorVariables class
+    %
+    % <a href="matlab:dash.doc('dash.serializedStateVectorVariables')">Documentation Page</a>
 
     % Prohibit serialized values from being altered
     properties (SetAccess = private)
@@ -36,6 +69,10 @@ classdef serializedStateVectorVariables
         svv = deserialize(obj);
     end
 
+    methods (Static)
+        tests;
+    end
+
     % Constructor
     methods
         function[obj] = serializedStateVectorVariables(svv)
@@ -65,6 +102,11 @@ classdef serializedStateVectorVariables
         %       obj (scalar serializedStateVectorVariable object): The serialized state vector variable.
         %
         % <a href="matlab:dash.doc('dash.serializedStateVectorVariable.serializedStateVectorVariable')">Documentation Page</a>
+
+        % Empty constructor option
+        if ~exist('svv','var') || isempty(svv)
+            return
+        end
         
         % Implement serialization in separate file to support separated
         % utility functions and error messages
