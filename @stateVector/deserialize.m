8abf4f5d8f394741862bff8a8c4c97706f485d27
JonKing93 <jonking93@gmail.com>
2022-02-23 16:20:20 -0700

    Convert sv.deserialize from static to object method

--- @stateVector/deserialize.m
+++ @stateVector/deserialize.m
@@ -1,43 +1,29 @@
-function[obj] = deserialize(s)
-%% stateVector.deserialize  Rebuild a stateVector object from a serialized struct
+function[obj] = deserialize(obj)
+%% stateVector.deserialize  Deserialize a state vector object
 % ----------
 %   obj = obj.deserialize
-%   Rebuilds a stateVector object from a serialized struct (produced by the
-%   stateVector.serialize method).
+%   Rebuilds a stateVector object from a serialized object. The
+%   deserialized object is valid for stateVector methods.
 % ----------
 %   Outputs:
 %       obj (scalar stateVector object): The deserialized stateVector object
 %
 % <a href="matlab:dash.doc('stateVector.deserialize')">Documentation Page</a>
 
-% Initialize object
-obj = stateVector;
-
-% Get directly copied fields
-props = string(properties(obj));
-copy = ~ismember(props, ["variables_","unused","subMembers"]);
-copy = props(copy);
-
-% Get copyable values
-for p = 1:numel(props)
-    field = props(p);
-    obj.(field) = s.(field);
-end
-
 % Rebuild unused members
-obj.unused = mat2cell(s.unused, s.nUnused, 1)';
+obj.unused = mat2cell(obj.unused, obj.nUnused, 1)';
 
 % Rebuild subMembers
-nSets = numel(s.nEnsDims);
-nElements = s.nMembers * s.nEnsDims;
-subMembers = mat2cell(s.subMembers, nElements, 1)';
+nSets = numel(obj.nEnsDims);
+nElements = obj.nMembers * obj.nEnsDims;
+subMembers = mat2cell(obj.subMembers, nElements, 1)';
 for k = 1:nSets
-    subMembers{k} = reshape(subMembers{k}, s.nMembers, s.nEnsDims(k));
+    subMembers{k} = reshape(subMembers{k}, obj.nMembers, obj.nEnsDims(k));
 end
 obj.subMembers = subMembers;
 
 % Rebuild variables
-obj.variables_ = dash.stateVectorVariable.deserialize(s.variables_);
+obj.variables_ = dash.stateVectorVariable.deserialize(obj.variables_);
 
 end
 
