76dcad53a8364909954a15cc3387e7c98b200abe
JonKing93 <jonking93@gmail.com>
2022-02-23 12:39:26 -0700

    Update meta.dispAttributes to standard style

--- @gridMetadata/dispAttributes.m
+++ @gridMetadata/dispAttributes.m
@@ -7,8 +7,35 @@ function[] = dispAttributes(obj)
 %
 % <a href="matlab:dash.doc('gridMetadata.dispAttributes')">Documentation Page</a>
 
+% Get the attributes structure
 dash.assert.scalarObj(obj, 'DASH:gridMetadata:dispAttributes');
 [~, atts] = obj.dimensions;
-disp(obj.(atts));
+attributes = obj.(atts);
+
+% Get attributes fields
+fields = string(fieldnames(attributes));
+nFields = numel(fields);
+
+% Exit if there are no fields
+if nFields==0
+    return
+end
+
+% Get whitespace padding for each field name
+widths = strlength(fields);
+pad = max(widths) - widths;
+
+% Cycle through fields
+for f = 1:numel(fields)
+    field = fields{f};
+    s = struct(field, attributes.(field));
+
+    % Print each field
+    padding = repmat(' ', 1, pad(f));
+    fprintf(['    ', padding]);
+    disp(s);
+    fprintf('\b');
+end
+fprintf('\n');
 
 end
