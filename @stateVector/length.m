6105e2e91dc59f2183713edd45bd0008dcffee1c
JonKing93 <jonking93@gmail.com>
2022-02-24 20:43:14 -0700

    Create method to return lengths

--- @stateVector/length.m
+++ @stateVector/length.m
@@ -1,27 +1,50 @@
-function[length] = length(obj)
+function[length] = length(obj, variables)
 %% stateVector.length  Return the length of a state vector
 % ----------
 %   length = <strong>obj.length</strong>
-%   Returns the number of elements in the state vector.
+%   length = <strong>obj.length</strong>(0)
+%   Return the number of elements in the state vector.
+%
+%   lengths = <strong>obj.length</strong>(-1)
+%   Return the number of elements for each variable in the state vector.
+%
+%   lengths = <strong>obj.length</strong>(v)
+%   lengths = <strong>obj.length</strong>(variableNames)
+%   Return the number of elements for the specified variables in the state
+%   vector.
 % ----------
+%   Inputs:
+%       v (logical vector | vector, linear indices): The indices
+%           of the variables for which to return state vector lengths.
+%       variableNames (string vector): The names of the variables in the 
+%           state vector for for which to return 
+%
 %   Outputs:
 %       length (scalar positive integer): The number of elements in the
 %           state vector.
+%       lengths (vector, positive integers): The number of state vector 
+%           elements for each of the specified variables.
 %
 % <a href="matlab:dash.doc('stateVector.length')">Documentation Page</a>
 
 % Setup
 header = "DASH:stateVector:length";
 dash.assert.scalarObj(obj, header);
-obj.assertUnserialized;
 
-% Sum the lengths of each variable
-length = 0;
-for v = 1:obj.nVariables
-    sizes = obj.variables_(v).stateSizes;
-    if ~isempty(sizes)
-        length = length + prod(sizes);
-    end
+% Parse variables
+if ~exist('variables','var') || isequal(variables,0)
+    v = 0;
+elseif isequal(variables, -1)
+    v = 1:obj.nVariables;
+else
+    v = obj.variableIndices(variables, true, header);
+end
+
+% Length of entire vector or variables
+if isequal(v,0)
+    length = sum(obj.lengths);
+else
+    length = obj.lengths(v);
 end
 
 end
