f0be3c503d814c4ffed1b188a84dc072fa85cd0e
JonKing93 <jonking93@gmail.com>
2022-03-04 14:40:56 -0700

    Minimized error stack in gridfileSources

--- +dash/@gridfileSources/indices.m
+++ +dash/@gridfileSources/indices.m
@@ -25,75 +25,84 @@ end
 nSources = numel(obj.source);
 
 % Error check direct indices
-if isnumeric(sources) || islogical(sources)
-    logicalRequirement = 'have one element per data source';
-    linearMax = 'the number of data sources';
-    indices = dash.assert.indices(sources, nSources, 'sources', logicalRequirement, linearMax, header);
-    
-% File name input - start by getting absolute paths to sources
-elseif dash.is.strlist(sources)
-    inputSources = string(sources);
-    sourcePaths = obj.absolutePaths;
-    
-    % Split source names from extensions
-    sourceNames = cellstr(sourcePaths);
-    sourceExt = cell(nSources, 1);
-    for s = 1:nSources
-        [path, name, ext] = fileparts(sourceNames{s});
-        sourceNames{s} = ['/',path,'/',name];
-        sourceExt{s} = ext;
-    end
-    
-    % Preallocate indices, use char comparisons. 
-    nInputs = numel(inputSources);
-    indices = NaN(nInputs, 1);
-    
-    % Check each input for an extension.
-    [~,~,ext] = fileparts(inputSources);
-    for k = 1:nInputs
-        hasExtension = ~strcmp(ext(k), "");
+try
+    if isnumeric(sources) || islogical(sources)
+        logicalRequirement = 'have one element per data source';
+        linearMax = 'the number of data sources';
+        indices = dash.assert.indices(sources, nSources, 'sources', logicalRequirement, linearMax, header);
         
-        % Clean input string and get char length
-        input = char(inputSources(k));
-        input = dash.file.urlSeparators(input);
-        if input(1)~='/'
-            input = strcat('/',input);
-        end
-        inputLength = length(input);
+    % File name input - start by getting absolute paths to sources
+    elseif dash.is.strlist(sources)
+        inputSources = string(sources);
+        sourcePaths = obj.absolutePaths;
         
-        % Get the source path (with or without extension) for comparison
+        % Split source names from extensions
+        sourceNames = cellstr(sourcePaths);
+        sourceExt = cell(nSources, 1);
         for s = 1:nSources
-            comparePath = sourceNames{s};
-            if hasExtension
-                comparePath = [comparePath, sourceExt{s}]; %#ok<AGROW>
+            [path, name, ext] = fileparts(sourceNames{s});
+            sourceNames{s} = ['/',path,'/',name];
+            sourceExt{s} = ext;
+        end
+        
+        % Preallocate indices, use char comparisons. 
+        nInputs = numel(inputSources);
+        indices = NaN(nInputs, 1);
+        
+        % Check each input for an extension.
+        [~,~,ext] = fileparts(inputSources);
+        for k = 1:nInputs
+            hasExtension = ~strcmp(ext(k), "");
+            
+            % Clean input string and get char length
+            input = char(inputSources(k));
+            input = dash.file.urlSeparators(input);
+            if input(1)~='/'
+                input = strcat('/',input);
+            end
+            inputLength = length(input);
+            
+            % Get the source path (with or without extension) for comparison
+            for s = 1:nSources
+                comparePath = sourceNames{s};
+                if hasExtension
+                    comparePath = [comparePath, sourceExt{s}]; %#ok<AGROW>
+                end
+                
+                % Check for a match.
+                match = false;
+                if length(comparePath)>=inputLength &&...
+                        strcmp(comparePath(end-inputLength+1:end), input)
+                    match = true;
+                end
+                
+                % Record matching source, but don't allow multiple matches
+                if match && isnan(indices(k))
+                    indices(k) = s;
+                elseif match
+                    multipleMatchesError(k, input, indices(k), s, obj.gridfile, header);
+                end
             end
             
-            % Check for a match.
-            match = false;
-            if length(comparePath)>=inputLength &&...
-                    strcmp(comparePath(end-inputLength+1:end), input)
-                match = true;
-            end
-            
-            % Record matching source, but don't allow multiple matches
-            if match && isnan(indices(k))
-                indices(k) = s;
-            elseif match
-                multipleMatchesError(k, input, indices(k), s, obj.gridfile, header);
+            % Throw error if there are no matches
+            if isnan(indices(k))
+                noMatchingSourcesError(k, input, obj.gridfile, header);
             end
         end
         
-        % Throw error if there are no matches
-        if isnan(indices(k))
-            noMatchingSourcesError(k, input, obj.gridfile, header);
-        end
+    % Any other type of input
+    else
+        id = sprintf('%s:invalidDataSource', header);
+        error(id, ['sources must either be a vector of data source indices, or ',...
+            'a list of data source names.']);
     end
-    
-% Any other type of input
-else
-    id = sprintf('%s:invalidDataSource', header);
-    error(id, ['sources must either be a vector of data source indices, or ',...
-        'a list of data source names.']);
+
+% Minimize error stacks
+catch ME
+    if ~contains(ME.identifier, "DASH:gridfile")
+        rethrow(ME);
+    end
+    throwAsCaller(ME);
 end
 
 end
