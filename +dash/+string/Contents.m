329fe4336a73f583075316e7f5103586f03a0240
JonKing93 <jonking93@gmail.com>
2022-03-28 12:36:26 -0700

    Test and clean dash.string

--- +dash/+string/Contents.m
+++ +dash/+string/Contents.m
@@ -1,10 +1,17 @@
-%% dash.string  Functions that manipulate strings
+%% dash.string  Functions that write strings
 % ----------
-% Functions:
-%   list        - List the elements of a vector in a formatted sting
-%   elementName - Returns an identifying name for an element in a list
+% Lists:
+%   list                - List the elements of a vector
+%   elementName         - Returns an identifying name for an element in a list
+%
+% Sizes:
+%   size                - Prints a size as a string of integers separated by xs
+%   nonscalarObj        - Return the disp header for a nonscalar DASH object
+%
+% Dimensions:
+%   indexedDimension    - Return names for use when indexing dimensions
 %
 % Unit Tests:
-%   tests       - Unit tests for the dash.string package
+%   tests               - Unit tests for the dash.string package
 %
 % <a href="matlab:dash.doc('dash.string')">Documentation Page</a>
--- +dash/+string/nonscalarObj.m
+++ +dash/+string/nonscalarObj.m
@@ -1,18 +1,40 @@
 function[info] = nonscalarObj(obj, type)
+%% dash.string.nonscalarObj  Return the disp header for a nonscalar DASH object
+% ----------
+%   info = dash.string.nonscalarObj(type)
+%   Returns the header for console display of a non-scalar DASH object.
+%   Includes the size or dimensionality of the array as appropriate.
+%   Includes an "empty" tag if the object is empty.
+% ----------
+%   Inputs:
+%       type (string scalar): The data type of the object
+%
+%   Outputs:
+%       info (string scalar): The header of the object as printed by a
+%           "disp" command.
+%
+%   <a href="matlab:dash.doc('dash.string.nonscalarObj')">Documentation Page</a>
 
+%%% Parameter
+maxDims = 4;
+%%%
+
+% Either print size or dimensionality
 N = ndims(obj);
-if N < 5
+if N <= maxDims
     siz = dash.string.size(size(obj));
 else
     siz = sprintf('%.f-D', N);
 end
 
+% Add "empty" tag to empty objects
 if isempty(obj)
     empty = 'empty ';
 else
     empty = '';
 end
 
+% Include "array" tag
 info = sprintf('  %s %s%s array\n\n', siz, empty, type);
 
 end
