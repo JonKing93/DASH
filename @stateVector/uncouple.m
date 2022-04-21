a54b03bc484b85d66595df14f9c1bcbbd80c0304
JonKing93 <jonking93@gmail.com>
2022-04-21 14:45:42 -0700

    Change uncoupling logic
    Uncouples listed variables from all other variables

--- @stateVector/uncouple.m
+++ @stateVector/uncouple.m
@@ -1,25 +1,26 @@
 function[obj] = uncouple(obj, variables)
-%% stateVector.uncouple  Allow variables to use different metadata when building ensemble members
+%% stateVector.uncouple  Uncouple variables in a state vector
 % ----------
 %   obj = obj.uncouple
 %   obj = obj.uncouple(-1)
-%   Uncouples all the variables in the state vector. Uncoupled variables
-%   are not required to have matching metadata within an ensemble member.
+%   Uncouples all the variables in the state vector from on another. The
+%   variables are not required to have matching metadata within individual
+%   ensemble members.
 %
 %   obj = obj.uncouple(v)
 %   obj = obj.uncouple(variableNames)
-%   Uncouples the specified variables from one another.
-%   Coupling is transitive, so unlisted variables that are coupled to two
-%   or more listed variables will also be uncoupled.
+%   Uncouples each of the listed variables from all other variables in the
+%   state vector. The uncoupled variables are not required to match the 
+%   metadata of other variables within individual ensemble members.
 % ----------
 %   Inputs:
 %       v (logical vector | linear indices | -1): The indices of variables in
-%           the state vector that should be coupled. Either a logical
+%           the state vector that should be uncoupled. Either a logical
 %           vector with one element per state vector variable, or a vector
 %           of linear indices. If -1, selects all variables in the state
 %           vector.
 %       variableNames (string vector): The names of variables in the state
-%           vector that should be coupled.
+%           vector that should be uncoupled.
 %
 %   Outputs:
 %       obj (scalar stateVector object): The state vector with updated
@@ -32,24 +33,15 @@ header = "DASH:stateVector:uncouple";
 dash.assert.scalarObj(obj, header);
 obj.assertEditable;
 
-% Check user variables, get indices
+% Get variable indices
 if ~exist('variables','var')
     variables = -1;
 end
-vUser = obj.variableIndices(variables, true, header);
+v = obj.variableIndices(variables, true, header);
 
-% Check each set of coupled variables for user-specified variables
-[sets, nSets] = obj.coupledIndices;
-for s = 1:nSets
-    vCoupled = sets{s};
-    isUserVar = ismember(vCoupled, vUser);
-
-    % If there are 2+ user variables in the set, uncouple the entire set
-    % (but maintain self coupling)
-    if sum(isUserVar)>1
-        obj.coupled(vCoupled,:) = false;
-        obj.coupled(1:obj.nVariables+1:end) = true;
-    end
-end
+% Uncouple
+obj.coupled(v,:) = false;
+obj.coupled(:,v) = false;
+obj.coupled(a:obj.nVariables+1:end) = true;
 
 end
