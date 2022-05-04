4afc0bc14ca5f246098c620b8eabece4f8d77921
JonKing93 <jonking93@gmail.com>
2022-05-04 12:44:05 -0700

    Wrote ensemble.lengths
    Scope should always come after variables.
    
    In most situations, cannot distinguish scope strings from variable strings.

--- @ensemble/length.m
+++ @ensemble/length.m
@@ -3,66 +3,84 @@ function[lengths] = length(obj, variables, scope)
 % ----------
 %   length = obj.length
 %   length = obj.length(0)
-%   Return the number of elements in the state vector for the ensemble
-%   object. If the "useVariables" command has been applied to the ensemble,
-%   only considers elements of the variables being used by the ensemble
-%   object.
+%   Returns the number of elements in the state vector for the ensemble. If
+%   the "useVariables" command has been applied to the ensemble, only
+%   counts elements of the variables being used by the ensemble.
 %
-%   lengths = obj.length(-1)
-%   Returns the number of state vector elements for each variable in the
-%   ensemble. If the "useVariables" command has been applied to the ensemble,
-%   only returns the lengths of variables being used by the ensemble object.
-%   The order of lengths corresponds to the order of variables being used
-%   by the ensemble object.
-%
-%   lengths = obj.length(v)
 %   lengths = obj.length(variableNames)
-%   Returns the lengths of the specified variables. If the "useVariables"
-%   command has been applied to the ensemble object, interprets variable
-%   names and indices in the context of variables being used by the
-%   ensemble object.
+%   lengths = obj.length(v)
+%   lengths = obj.length(-1)
+%   Returns the lengths of the specified variables. Variable names and
+%   indices are interpreted in the context of variables being used by the
+%   ensemble. If the index is -1, selects all variables being used.
 %
-%   ... = obj.length(..., scope)
-%   ... = obj.length(..., false|"u"|"used")
-%   ... = obj.length(...,  true|"f"|"file")
-%   Indicates the scope in which to consider variables. If false,
-%   interprets values in the context of variables being used by the
-%   ensemble object. This option behaves identically to the previous
-%   syntaxes. 
-% 
-%   If true, interprets values in the context of all variables saved in the
-%   .ens file, including variables not being used by the ensemble object.
-%   If the first input is 0, returns the lengths of the full state vector
-%   saved in the .ens file. If the first input is -1, returns the lengths
-%   of all variables saved in the .ens file. Otherwise, interprets variable
-%   names and indices in the context of all variables saved in the .ens file.
+%   length = obj.length(0, scope)
+%   ... = obj.length(0, "used"|"u"|false)
+%   ... = obj.length(0, "file"|"f"|true)
+%   Indicate the scope in which to return the length of the state vector.
+%   If "used"|"u"|false, counts elements in variables being used by the
+%   ensemble. If "file"|"f"|true, counts elements in all variables saved in
+%   the .ens file.
+%
+%   lengths = obj.length(variables, scope)
+%   ... = obj.length(variables, "used"|"u"|false)
+%   ... = obj.length(variables, "file"|"f"|true)
+%   Indicate the scope in which to interpret variable names and indices. If
+%   "used"|"u"|false, interprets names and indices in the context of
+%   variables being used by the state vector. If "file"|"f"|true,
+%   interprets names and indices in the context of all variables saved in
+%   the .ens file.
 % ----------
+% ----------
+%   Inputs:
+%       v (0 | -1 | vector, linear indices): The indices of variables for
+%           which to return lengths. If 0, returns the full length of the
+%           state vector, rather than individual variable lengths. If -1, returns
+%           the lengths of every variable. Depending on scope, these
+%           indices are interpreted with respect to either the variables
+%           being used by the ensemble, or the variables saved in the .ens file.
+%       variableNames (string vector): The names of variables for which to
+%           return lengths. Depending on scope, these names must either be
+%           the names of used variables, or the names of variables saved in
+%           the .ens file.
+%       scope (string scalar | logical scalar): The scope in which to
+%           interpret indices and return lengths
+%           ["used"|"u"|true (default)]: Return lengths relative to the set
+%               of variables being used by the ensemble object.
+%           ["file"|"f|false]: Return lengths relative to the set of all
+%               variables stored in the .ens file.
+%
+%   Outputs:
+%       length (scalar integer): The total length of the state vector.
+%       lengths (vector, integers): The lengths of the specified variables.
+%
+% <a href="matlab:dash.doc('ensemble.length')">Documentation Page</a>
 
 % Setup
 header = "DASH:ensemble:length";
 dash.assert.scalarObj(obj, header);
 
 % Defaults
-if ~exist('variables','var') || isempty(variables)
+if ~exist('variables','var')
     variables = 0;
 end
-if ~exist('scope','var') || isempty(scope)
-    scope = false;
+if ~exist('scope', 'var')
+    scope = "used";
 end
 
-% Note whether to sum over variables
-returnSum = false;
+% Note if returning the sum of variable lengths
+takeSum = false;
 if isequal(variables, 0)
-    returnSum = true;
+    takeSum = true;
     variables = -1;
 end
 
-% Parse scope and variables.
-v = obj.variableIndices(variables, scope);
+% Parse indices and scope
+v = obj.variableIndices(variables, scope, header);
 
-% Get lengths, optionally take sum
+% Get lengths and optionally take sum
 lengths = obj.lengths(v);
-if returnSum
+if takeSum
     lengths = sum(lengths);
 end
 
