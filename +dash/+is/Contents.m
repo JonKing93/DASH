6da72c75c8044759aa23078a48a72da1156bb27c
JonKing93 <jonking93@gmail.com>
2022-05-13 11:32:06 -0700

    Fix indenting

--- +dash/+is/Contents.m
+++ +dash/+is/Contents.m
@@ -1,21 +1,21 @@
 %% dash.is  Functions that test if inputs meet a condition
 % ----------
 % Strings:
-%   charrow          - True for a character row vector
-%   str              - True for a string or cellstring data type
-%   string           - True for string, cellstring, or character row vector
-%   strflag          - True for string scalar, cellstring scalar, or character row vector
-%   strlist          - True for a string vector, cellstring vector, or character row vector
+%   charrow             - True for a character row vector
+%   str                 - True for a string or cellstring data type
+%   string              - True for string, cellstring, or character row vector
+%   strflag             - True for string scalar, cellstring scalar, or character row vector
+%   strlist             - True for a string vector, cellstring vector, or character row vector
 %
 % Numeric:
-%   integers         - Test if numeric array consists entirely of integers
-%   postiveIntegers  - Test if numeric array consists entirely of positive integers
+%   integers            - Test if numeric array consists entirely of integers
+%   positiveIntegers    - Test if numeric array consists entirely of positive integers
 %
 % Misc:
-%   url              - True if string begins with http:// or https://
-%   uniqueSet        - Test if input contains repeated values
+%   url                 - True if string begins with http:// or https://
+%   uniqueSet           - Test if input contains repeated values
 %
 % Tests:
-%   tests            - Unit tests for the dash.is package
+%   tests               - Unit tests for the dash.is package
 %
 % <a href="matlab:dash.doc('dash.is')">Documentation Page</a>
