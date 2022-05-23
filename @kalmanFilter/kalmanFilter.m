b226038bf24abb4eef000581350003ffa8305bad
JonKing93 <jonking93@gmail.com>
2022-05-23 14:18:32 -0700

    ...

--- @kalmanFilter/kalmanFilter.m
+++ @kalmanFilter/kalmanFilter.m
@@ -1,131 +1,64 @@
-classdef kalmanFilter < ensembleFilter
-    %% Implements a Kalman Filter analysis using an ensemble square root Kalman Filter
-    %
-    % kalmanFilter Methods:
-    %   kalmanFilter - Creates a new kalmanFilter object
-    %   
-    %   observations - Sets the proxy observations and uncertainties
-    %   prior - Sets the prior ensemble
-    %   estimates - Sets the proxy estimates
-    %
-    %   variance - Enable or disable posterior ensemble variance as output
-    %   percentiles - Return posterior ensemble percentiles as output
-    %   deviations - Return posterior ensemble deviations as output
-    %   index - Returns the posterior for an index as output
-    %   mean - Enable or disable the ensemble mean as output
-    %
-    %   localize - Implement covariance localization
-    %   blend - Implement covariance blending
-    %   inflate - Apply covariance inflation
-    %   setCovariance - Specify covariance estimates directly
-    %   resetCovariance - Restore covariance options to defaults
-    %   covarianceEstimate - Return the covariance estimate for a given assimilated time step
-    
-    % ----- Written By -----
-    % Jonathan King, University of Arizona, 2019-2020
-    
-    properties (SetAccess = private)
-        %% Covariance adjustments
-        
-        % Inflation
-        inflateCov = false; % True or false toggle
-        inflateFactor; % Inflation factor
-        whichFactor; % Which factor to use in each time step
-        
-        % Localization
-        localizeCov = false; % Logical toggle
-        wloc; % State vector localization weights
-        yloc; % Y Localization weights
-        whichLoc; % Which localization to use in each time step
-        
-        % Blending
-        setCov = false; % Toggle for setting C directly
-        blendCov = false; % Toggle for blending C
-        C; % State vector-proxy covariance matrix
-        Ycov; % Y covariance matrix
-        whichCov; % Which covariance to use in each time step
-        blendWeights; % Blending weights
-        
-        %% Output options
-        
-        % Basic returns
-        return_mean; % Whether to return the posterior mean
-        return_devs; % Whether to return the posterior deviations
-        
-        % Posterior calculations
-        Q = cell(0,1); % Posterior calculations
-        Qname = strings(0,1); % The output name of each posterior calculation
-        
-        % Saving to file
-        file; % Save file
-        overwrite; % Overwrite permission
-    end
-        
+classdef kalmanFilter < dash.ensembleFilter
+
+
+
+
     % Constructor
     methods
-        function[kf] = kalmanFilter(name)
-            %% Creates a new kalmanFilter object
+        function[obj] = kalmanFilter(varargin)
+            %% kalmanFilter.kalmanFilter  Create a new kalmanFilter object
+            % ----------
+            %   obj = kalmanFilter
+            %   Initializes a new Kalman filter object. The new object
+            %   does not have observations, estimates, uncertainties, or a
+            %   prior. The algorithm will only return the ensemble mean
+            %   when run.
             %
-            % kf = kalmanFilter;
-            % Creates a new kalman filter
+            %   obj = kalmanFilter(label)
+            %   obj = kalmanFilter(labels)
+            %   Also applies a label to the Kalman filter object. If
+            %   unset, the label is set to an empty string. If labels is a
+            %   string array, returns an array of Kalman filter objects
+            %   and applies the corresponding label to each individual
+            %   Kalman filter.
             %
-            % kf = kalmanFilter(name)
-            % Optionally gives the kalman filter an identifying name.
+            %   obj = kalmanFilter(size)
+            %   obj = kalmanFilter(size, labels)
+            %   Initializes an array of Kalman filter objects of the
+            %   indicated size. Optionally also applies a label to each
+            %   object in the array. If applying labels, the size of the
+            %   "labels" array must match the requested size of the
+            %   kalmanFilter array.
+            % ----------
+            %   Inputs:
+            %       labels (string array | cellstring array | char row vector): Labels
+            %           for the elements of a kalmanFilter array. If the only input,
+            %           the output array will have the same size as labels. If combined
+            %           with the "size input, must be the size of the requested array.
+            %       size (row vector, positive integers): Indicates the size of the
+            %           requested array of kalmanFilter objects. Must have at least 2
+            %           elements, and all elements must be non-negative integers.
             %
-            % ----- Inputs -----
+            %   Outputs:
+            %       obj (scalar kalmanFilter object | kalmanFilter array): A new
+            %           kalmanFilter object or an array of kalmanFilter objects.
             %
-            % name: An optional name for the Kalman filter. A string.
-            %
-            % ----- Outputs -----
-            %
-            % kf: The new kalman filter object
+            % <a href="matlab:dash.doc('kalmanFilter.kalmanFilter')">Documentation Page</a>
             
-            % Name            
-            if ~exist('name','var') || isempty(name)
-                name = "";
-            end
-            kf = kf.rename(name);
-            
-            % Return mean and variance by default
-            kf.return_mean = true;
-            kf.return_devs = false;
-            kf = kf.variance(true);
+            % Header
+            header = "DASH:kalmanFilter";
+
+            % Parse inputs
+            [siz, labels] = dash.parse.constructorInputs(varargin, header);
+
+            % Build kalman filter array
+            obj = repmat(obj, siz);
+
+            % Apply labels
+            dash.assert.sameSize(labels, obj, 'the "labels" input', 'the requested kalmanFilter array', header);
+            labels = num2cell(labels);
+            [obj.label_] = labels{:};
         end
     end
-    
-    % ensembleFilter methods
-    methods
-        kf = prior(kf, M, whichPrior);
-        kf = observations(kf, D, R, isCovariance);
-        out = run(kf, showprogress, complexError);
-    end
 
-    % User output options
-    methods
-        kf = mean(kf, tf);
-        kf = deviations(kf, tf);
-        kf = percentiles(kf, percs);
-        kf = variance(kf, tf);
-        kf = index(kf, name, weights, rows, nanflag);
-    end
-    
-    % User covariance methods
-    methods
-        kf = inflate(kf, factor, whichFactor);
-        kf = localize(kf, wloc, yloc, whichLoc);
-        kf = blend(kf, C, Ycov, weights, whichCov);
-        kf = setCovariance(kf, C, Ycov, whichCov);
-        kf = resetCovariance(kf);
-        [C, Ycov] = covarianceEstimate(kf, t);
-    end
-    
-    % Utilities
-    methods
-        checkSize(kf, siz, type, dim, inputName);
-        assertEditableCovariance(kf, type);
-        [whichCov, nCov] = checkCovariance(kf, C, Ycov, whichCov, locInputs);
-        kf = finalize(kf);
-        [Knum, Ycov] = estimateCovariance(kf, t, Mdev, Ydev);
-    end    
-    
 end
---
+++ @kalmanFilter/label.m
@@ -0,0 +1,32 @@
+function[varargout] = label(obj, label)
+%% kalmanFilter.label  Return or set the label of an kalmanFilter object
+% ----------
+%   label = obj.label
+%   Returns the label of the current filter.
+%
+%   obj = obj.label(label)
+%   Applies a new label to the filter.
+% ----------
+%   Inputs:
+%       label (string scalar): A new label for the filter
+%
+%   Outputs:
+%       label (string scalar): The current label of the filter
+%       obj (scalar kalmanFilter object): The Kalman filter with an updated label.
+%
+% <a href="matlab:dash.doc('kalmanFilter.label')">Documentation Page</a>
+
+% Header
+header = "DASH:kalmanFilter:label";
+
+% Use cell wrapper for inputs
+if ~exist('label','var')
+    label = {};
+else
+    label = {label};
+end
+
+% Parse label
+varargout = label@dash.ensembleFilter(obj, header, label{:});
+
+end
