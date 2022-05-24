fc6f334e9399abf7276e7dcf14b2cf77b590ced8
JonKing93 <jonking93@gmail.com>
2022-05-24 12:20:10 -0700

    Update observations.m

--- +dash/@ensembleFilter/observations.m
+++ +dash/@ensembleFilter/observations.m
@@ -1,14 +1,41 @@
 function[outputs, type] = observations(obj, header, Y)
-%% dash.ensembleFilter.observations  Provide the observations for a filter
+%% dash.ensembleFilter.observations  Provide the observations for a filter object
 % ----------
+%   [outputs, type] = obj.observations(header, ...)
+%   Processes options for observations for a filter object. Returns the
+%   appropriate outputs collected in a cell, and a string indicating the
+%   type of operation performed.
+%
+%   [obsCell, 'return'] = obj.observations(header)
+%   Returns the current observation matrix.
+%
+%   [objCell, 'set'] = obj.observations(header, Y)
+%   Error checks the input observation matrix and overwrites any previously
+%   existing observations. Returns the updated filter object.
+%
+%   [objCell, 'delete'] = obj.observations(header, 'delete')
+%   Deletes any current observations and returns the updated filter object.
+% ----------
+%   Inputs:
+%       header (string scalar): Header for thrown error IDs
+%       Y (numeric matrix [nSite x nTime]): Observations for the filter
+%
+%   Outputs:
+%       outputs (cell scalar): Varargout-style outputs
+%       type ('return'|'set'|'delete'): Indicates the type of operation
+%       obsCell (cell scalar {Y}): The current observations in a cell
+%       objCell (cell scalar {obj}): The updated object in a cell
+%
+% <a href="matlab:dash.doc('dash.ensembleFilter.observations')">Documentation Page</a>
 
-% Default
-if ~exist('header','var') || isempty(header)
-    header = "DASH:ensembleFilter:observations";
-end
-
-% Return observations matrix
+% Setup
 try
+    if ~exist('header','var') || isempty(header)
+        header = "DASH:ensembleFilter:observations";
+    end
+    dash.assert.scalarObj(obj, header);
+
+    % Return observations matrix
     if ~exist('Y','var')
         outputs = {obj.Y};
         type = 'return';
@@ -33,7 +60,7 @@ try
 
         % Do initial error checks
         name = 'Observations (Y)';
-        dash.assert.matrixTypeSize(Y, 'numeric', [], name, header);
+        dash.assert.matrixTypeSize(Y, ["single","double"], [], name, header);
         dash.assert.defined(Y, 1, name, header);
         
         % Get size of matrix
@@ -94,5 +121,10 @@ id = sprintf('%s:incorrectNumberOfTimeSteps', header);
 ME = MException(id, ['You previously specified %s for %.f time steps, so the ',...
     'observations matrix (Y) must have %.f columns. However, Y has %.f columns instead.'],...
     type, obj.nTime, obj.nTime, nTime);
+throwAsCaller(ME);
+end
+function[] = emptyObservationsError(obj, header)
+id = sprintf('%s:emptyObservations', header);
+ME = MException(id, 'The observations for %s cannot be empty.', obj.name);
 throwAsCaller(ME);
 end
