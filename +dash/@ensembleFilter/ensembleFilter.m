69c1aba8a05bf3fb0517903b01d98c54a52bc524
JonKing93 <jonking93@gmail.com>
2022-05-24 12:20:32 -0700

    Update ensembleFilter.m

--- +dash/@ensembleFilter/ensembleFilter.m
+++ +dash/@ensembleFilter/ensembleFilter.m
@@ -22,12 +22,15 @@ classdef (Abstract) ensembleFilter
         
         Y;                  % Proxy records / observations
         Ye;                 % Proxy estimates / observation estimates
+
         X;                  % The prior
+        Xtype = NaN         % Indicates the type of prior: NaN-Unset, 0-numeric array, 1-scalar ensemble object, 2-vector of ensemble objects
+        precision = "";     % Indicates the numerical precision of the prior
         whichPrior          % Indicates which prior to use in each assimilated time step
 
         R;                  % Proxy uncertainties / observation uncertainties
+        Rtype = NaN;        % Indicates the type of uncertainties: NaN-Unset, 0-error variances, 1-error covariances
         whichR;             % Indicates which set of uncertainties to use in each assimilated time step
-        Rtype;              % Indicates the type of uncertainties: 0-error variances, 1-error covariances
 
     end
 
@@ -35,12 +38,22 @@ classdef (Abstract) ensembleFilter
 
         % General
         varargout = label(varargin);
+        obj = finalize(obj, requirePrior, actionName, header);
 
         % Essential inputs
         [outputs, type] = observations(obj, header, Y);
         [outputs, type] = prior(obj, header, X, whichPrior);
         [outputs, type] = estimates(obj, header, Ye, whichPrior);
         [outputs, type] = uncertainties(obj, header, R, whichR)
+
+        % Input utilities
+        assertValidR(obj, header);
+        obj = processWhich(obj, whichArg, field, nIndex, indexType, ...
+                                            timeIsSet, whichIsSet, header);
+
+        % Query values
+        Rcov = Rcovariance(t, s);
+        X = loadPrior(p);
         
     end
 
