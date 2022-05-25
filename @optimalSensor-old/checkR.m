5a722bb1249b60caaea7ec3192ed4f575f6082f3
JonKing93 <jonking93@gmail.com>
2022-05-25 13:19:42 -0700

    remove old sensor methods

--- @optimalSensor-old/checkR.m
+++
@@ -1,9 +0,0 @@
-function[] = checkR(R, nSite)
-% Error checks R inputs
-
-dash.assert.vectorTypeN(R, 'numeric', nSite, 'R');
-dash.assert.realDefined(R, 'R');
-assert(~any(R<=0), 'R can only include positive values');
-
-end
-
