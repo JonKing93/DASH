726ad5f4060a68dade2cfb08f7b2d83432f6edee
JonKing93 <jonking93@gmail.com>
2022-05-27 11:07:25 -0700

    fix varargout in filter.label

--- +dash/@ensembleFilter/label.m
+++ +dash/@ensembleFilter/label.m
@@ -1,33 +1,36 @@
-function[varargout] = label(obj, label)
+function[outputs] = label(obj, header, label)
 %% dash.ensembleFilter.label  Return or set the label of an filter object
 % ----------
-%   label = obj.label
+%   labelCell = obj.label(header)
 %   Returns the label of the current filter.
 %
-%   obj = obj.label(label)
+%   objCell = obj.label(header, label)
 %   Applies a new label to the filter.
 % ----------
 %   Inputs:
+%       header (string scalar): Header for thrown error IDs
 %       label (string scalar): A new label for the filter
 %
 %   Outputs:
-%       label (string scalar): The current label of the filter
-%       obj (scalar ensembleFilter object): The filter with an updated label.
+%       labelCell (scalar cell {label}): The current label of the filter in a cell
+%       obj (scalar cell {obj}): The filter with an updated label in a cell
 %
 % <a href="matlab:dash.doc('dash.ensembleFilter.label')">Documentation Page</a>
 
 % Setup
-header = "DASH:ensembleFilter:label";
+if ~exist('header','var')
+    header = "DASH:ensembleFilter:label";
+end
 dash.assert.scalarObj(obj, header);
 
 % Return current label
 if ~exist('label','var')
-    varargout = {obj.label_};
+    outputs = {obj.label_};
 
 % Apply new label
 else
     obj.label_ = dash.assert.strflag(label, 'label', header);
-    varargout = {obj};
+    outputs = {obj};
 end
 
 end
