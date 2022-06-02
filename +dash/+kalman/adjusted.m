d67cb0ed254bb62a688f6223ca28ca77a2043adc
Jonathan King <jonking93@gmail.com>
2022-06-02 15:02:31 -0700

    Document kalman math

--- +dash/+kalman/adjusted.m
+++ +dash/+kalman/adjusted.m
@@ -1,5 +1,25 @@
-function[a] = adjusted(R, Kdenom)
+function[Ka] = adjusted(Knum, Kdenom, Rcov)
+%% dash.kalman.adjusted  Computes the adjusted Kalman gain for updating ensemble deviations
+% ----------
+%   Ka = dash.kalman.adjusted(Knum, Kdenom, Rcov)
+%   Computes the adjusted Kalman gain for updating ensemble deviations.
+% ----------
+%   Inputs:
+%       Knum (numeric matrix, [nState x nSite]): The numberator of the
+%           Kalman gain. This is the covariance of the state vector
+%           elements with the observation estimates.
+%       Kdenom (symmetric numeric matrix, [nSite x nSite]): The denominator
+%           of the Kalman Gain. This is the observation covariances plus
+%           the R error covariances.
+%       Rcov (symmetric numeric matrix, [nSite x nSite]): The R error covariances
+%
+%   Outputs:
+%       Ka (numeric matrix, [nState x nSite]): The adjusted Kalman gain for
+%           updating ensemble deviations.
+%
+% <a href="matlab:dash.doc('dash.kalman.adjusted')">Documentation Page</a>
 
-a = 1 / (1 + sqrt(R / Kdenom));
+Ksqrt = sqrtm(Kdenom);
+Ka = Knum * (Ksqrt^-1)' * (Ksqrt + sqrtm(Rcov))^-1;
 
 end
--- +dash/+kalman/denominator.m
+++ +dash/+kalman/denominator.m
@@ -1,5 +1,23 @@
-function[Kdenom] = denominator(Ydev, Rcov, unbias)
+function[Kdenom] = denominator(Ycov, Rcov)
+%% dash.kalman.denominator  Computes the denominator of the Kalman Gain
+% ----------
+%   Kdenom = dash.kalman.denominator(Ycov, Rcov)
+%   Computes the denominator of the Kalman Gain. This is the sum of the R
+%   uncertainty error-covariances and the covariances between the
+%   observation estimates.
+% ----------
+%   Inputs:
+%       Ycov (symmetric numeric matrix [nSite x nSite]): The covariances of
+%           the observation estimates with one another.
+%       Rcov (symmetric numeric matrix [nSite x nSite]): The R uncertainty
+%           error covariances
+%
+%   Outputs:
+%       Kdenom (symmetric numeric matrix [nSite x nSite]): The denominator
+%           of the Kalman gain.
+%
+% <a href="matlab:dash.doc('dash.kalman.denominator')">Documentation Page</a>
 
-Kdenom = unbias .* (Ydev * Ydev') + Rcov;
+Kdenom = Ycov + Rcov;
 
 end
