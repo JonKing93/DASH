64e1c26726c25c4bbe8c7f889fd72462c426a3c1
Jonathan King <jonking93@gmail.com>
2022-06-23 08:44:32 -0700

    simplify os.evaluate

--- @optimalSensor/evaluate.m
+++ @optimalSensor/evaluate.m
@@ -1,14 +1,14 @@
-function[variance, metric] = evaluate(obj)
+function[deltaVariance] = evaluate(obj)
 %% optimalSensor.evaluate  Evaluates the variance reduced by individual proxies in a network
 % ----------
-%   [variance, metric] = obj.evaluate
+%   deltaVariance = obj.evaluate
 %   For each observation site, evaluates the amount of the metric's
 %   variance that is reduced when the site is assimilated alone. Evaluates
-%   the *individual* influence of proxy observations on an assimilation.
+%   the *individual* influence of each proxy observation on an assmilation.
 %   
 %   **Important**
 %   This method neglects the covariance between observation sites, and only
-%   considers the effects of assimilating site individually. In general,
+%   considers the effects of assimilating each site individually. In general,
 %   when observation estimates covary, their total effect on the metric's
 %   variance will be smaller than the sum of their individual effects. Use
 %   the "optimalSensor.update" method to assess the total effect on metric
@@ -17,14 +17,8 @@ function[variance, metric] = evaluate(obj)
 %   when using R uncertainty variances.
 % ----------
 %   Outputs:
-%       variance (scalar struct): Reports the variance of the metric across
-%           the ensemble. Has the following fields.
-%           .initial (numeric scalar): The initial variance of the metric
-%           .delta (numeric vector [nSites]): Reports the reduction of
-%               variance that occurs when assimilating each site individually
-%       metric (scalar struct): Reports the metric. Has the fields:
-%           .initial (numeric vector [nMembers]): The initial metric
-%               extracted from the prior.
+%       deltaVariance (numeric vector [nSites]): The reduction of variance
+%           that occurs when assimilating each site individually.
 %   
 % <a href="matlab:dash.doc('optimalSensor.evalute')">Documentation Page</a>
 
@@ -35,26 +29,13 @@ dash.assert.scalarObj(obj, header);
 % Require essential data inputs
 obj.assertFinalized;
 
-% Initialize outputs
-variance = struct;
-metric = struct;
-
-% Coefficient for unbiased estimator
-unbias = dash.math.unbias(obj.nMembers);
-
-% Get the metric, its deviations, and its variance
-J = obj.computeMetric;
-[~, Jdev] = dash.math.decompose(J);
-Jvar = dash.math.variance(Jdev, unbias);
-
-metric.initial = J;
-variance.initial = Jvar;
-
-% Get R variances
-R = obj.Rvariances;
-
-% Decompose the estimates and get their effect on variance
+% Decompose the metric and the estimates
+[~, Jdev] = dash.math.decompose(obj.J);
 [~, Ydev] = dash.math.decompose(obj.Ye);
-variance.delta = obj.varianceReduction(Jdev, Ydev, R, unbias);
+
+% Assess the effect of each site on metric variance
+unbias = dash.math.unbias(obj.nMembers);
+Rvar = obj.Rvariances;
+deltaVariance = obj.varianceReduction(Jdev, Ydev, Rvar, unbias);
 
 end
