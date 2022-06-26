1118754fd6e79761d1f3a9d7f249d0f65ddb327c
Jonathan King <jonking93@gmail.com>
2022-06-25 17:36:05 -0700

    wrote PSM info methods

---
+++ +PSM/githubInfo.m
@@ -0,0 +1,54 @@
+function[info] = githubInfo(psmNames)
+%% PSM.githubInfo  Return information about the Github repository for each PSM
+% ----------
+%   info = PSM.githubInfo
+%   Returns information about the Github repository and commit for each PSM
+%   supported by the DASH toolbox. Information includes the Github
+%   repository, the commit hash of the supported version, and a comment
+%   about the commit.
+%
+%   info = PSM.githubInfo(psmNames)
+%   Returns information about the specified PSMs.
+% ----------
+%   Inputs:
+%       psmNames (string vector [nNames]): The names of the PSMs for which
+%           to return information.
+%
+%   Outputs:
+%       info (table [nNames x 4]): A table with information about the specified
+%           PSMs. Includes PSM names, Github repository, commit hash of 
+%           the supported version, and a comment about the commit.
+%
+% <a href="matlab:dash.doc('PSM.githubInfo')">Documentation Page</a>
+
+% Setup
+header = "DASH:PSM:githubInfo";
+supported = PSM.supported;
+
+% Default and error check PSM names
+if ~exist('psmNames', 'var')
+    psmNames = PSM.supported;
+else
+    psmNames = dash.assert.strlist(psmNames, 'psmNames', header);
+    dash.assert.strsInList(psmNames, supported, 'psmNames', 'the name of a supported PSM', header);
+end
+
+% Preallocate
+nNames = numel(psmNames);
+repositories = strings(nNames, 1);
+commits = strings(nNames, 1);
+comments = strings(nNames, 1);
+
+% Get information
+for n = 1:nNames
+    name = psmNames(n);
+    repositories(n) = PSM.(name).repository;
+    commits(n) = PSM.(name).commit;
+    comments(n) = PSM.(name).commitComment;
+end
+
+% Organize as a table
+info = table(psmNames(:), repositories, commits, comments);
+info.Properties.VariableNames = ["Name", "Repository", "Commit", "Comment"];
+
+end
---
+++ +PSM/info.m
@@ -0,0 +1,51 @@
+function[info] = info(psmNames)
+%% PSM.info  Return information about the PSMs supported by DASH
+% ----------
+%   info = PSM.info
+%   Return information about all the PSMs supported by the DASH toolbox.
+%   Information includes PSM names, descriptions, and whether each PSM can
+%   estimate R uncertainties.
+%
+%   info = PSM.info(psmNames)
+%   Returns information about the specified PSMs.
+% ----------
+%   Inputs:
+%       psmNames (string vector [nNames]): The names of the PSMs for which
+%           to return information.
+%
+%   Outputs:
+%       info (table [nNames x 3]): A table with information about the specified
+%           PSMs. Includes PSM names, descriptions, and whether each PSM
+%           can estimate R uncertainties.
+%
+% <a href="matlab:dash.doc('PSM.info')">Documentation Page</a>
+
+% Setup
+header = "DASH:PSM:info";
+supported = PSM.supported;
+
+% Default and error check PSM names
+if ~exist('psmNames', 'var')
+    psmNames = PSM.supported;
+else
+    psmNames = dash.assert.strlist(psmNames, 'psmNames', header);
+    dash.assert.strsInList(psmNames, supported, 'psmNames', 'the name of a supported PSM', header);
+end
+
+% Preallocate
+nNames = numel(psmNames);
+descriptions = strings(nNames, 1);
+estimatesR = false(nNames, 1);
+
+% Get information
+for n = 1:nNames
+    name = psmNames(n);
+    descriptions(n) = PSM.(name).description;
+    estimatesR(n) = PSM.(name).estimatesR;
+end
+
+% Organize as a table
+info = table(psmNames(:), descriptions, estimatesR);
+info.Properties.VariableNames = ["Name", "Description","Can_Estimate_R"];
+
+end
