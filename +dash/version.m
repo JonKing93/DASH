6cca2cda71f1b2e583ff9baab6ff222aa7944681
Jonathan King <jonking93@gmail.com>
2022-06-28 19:14:41 -0700

    Update version

--- +dash/version.m
+++ +dash/version.m
@@ -10,7 +10,7 @@ function[varargout] = version
 %   Outputs:
 %       v (string scalar): The current version string of the DASH toolbox
 %
-% <a href="matlab:dash.doc('dash.version')">Online Documentation</a>
+% <a href="matlab:dash.doc('dash.version')">Documentation Page</a>
 
 version = "v4.0.0-beta-1.0";
 if nargout==0
