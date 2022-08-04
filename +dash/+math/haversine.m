3412725719ca4d24108e415c026b14b6df196a44
Jonathan King <jonking93@gmail.com>
2022-08-04 09:57:30 -0700

    Tested haversine rounding error fix

--- +dash/+math/haversine.m
+++ +dash/+math/haversine.m
@@ -57,7 +57,7 @@ dLat = coordinates1(:,1) - coordinates2(1,:);
 dLon = coordinates1(:,2) - coordinates2(2,:);
 
 % Get haversine function of the central angle. (Account for rounding errors
-% that might allow *a* to become greater than 1)
+% that might allow *a* to become slightly greater than 1)
 a = sin(dLat/2).^2 + ( cos(coordinates1(:,1)) .* cos(coordinates2(1,:)) .* sin(dLon/2).^2 );
 a(a>1) = 1;
 c = 2 * atan2( sqrt(a), sqrt(1-a) );
