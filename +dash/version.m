3ff241ca004d9a45cf75139de932e99b06a8c437
Jonathan King <jonking93@gmail.com>
2022-08-04 10:05:01 -0700

    Update version.m

--- +dash/version.m
+++ +dash/version.m
@@ -12,7 +12,7 @@ function[varargout] = version
 %
 % <a href="matlab:dash.doc('dash.version')">Documentation Page</a>
 
-version = "v4.0.0-beta-1.0";
+version = "v4.0.0-beta-1.1";
 if nargout==0
     disp(version);
 else
