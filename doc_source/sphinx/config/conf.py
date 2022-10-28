e12300423b933833a614834a56cfc435572fb76e
Jonathan King <jonking93@gmail.com>
2022-10-28 08:40:39 -0700

    Update conf.py with css classes

--- doc_source/sphinx/config/conf.py
+++ doc_source/sphinx/config/conf.py
@@ -31,6 +31,7 @@ release = '4.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -52,13 +53,14 @@ pygments_style = 'matlab'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ["_static"]
+html_css_files = ["css/matlab-io.css","css/accordion.css"]
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_copy_source = False
+
 
 # The HTML header
 html_context = {
   'display_github': True
 }
+github_url = "https://github.com/JonKing93/DASH"
