f59eb346793c7162056a6ea895b91a0df30a12fc
Jonathan King <jonking93@gmail.com>
2022-10-28 07:16:29 -0700

    Add Python installation doc to PSM.prysm

--- +PSM/+prysm/Contents.m
+++ +PSM/+prysm/Contents.m
@@ -1,7 +1,43 @@
 %% PSM.prysm  Classes that implement PRYSM sensor models
 % ----------
 %   The PSM.prysm package implements classes that run PRYSM sensor models.
-%   These packages are concerte classes of the PSM.prysm superclass.
+%   Each class implements a specific PRSYM sensor model - the names of the
+%   classes in DASH match the names of the corresponding PSM module in
+%   PRYSM. A list of supported PRYSM forward models is given below.
+%
+%   DOWNLOADING:
+%   The PRYSM forward models are all contained in the same package. Thus,
+%   if you use the "PSM.download" command to download a specific forward
+%   models, then you will also download all the other forward models in the
+%   PRYSM suite.
+%
+%   SETTING UP PRYSM:
+%   The PRYSM forward models are written in Python, and so require some
+%   extra setup to run in Matlab. You will need to complete the following to
+%   run the PRYSM forward models in DASH:
+%   1. Install Python 3.7, 3.8, or 3.9 **directly** on your computer. You
+%      DO NOT use conda to install python, as Matlab is unable to locate
+%      Python within a conda distribution. Instead, download the language
+%      from python.org and install it.
+%   2. Use pip to install numpy, and scipy. Do this in a terminal (and not
+%      the Matlab console). For example:
+%      $ pip install numpy
+%      $ pip install scipy
+%   3. Use pip to install the PRYSM package. Again, this should be in your
+%      computer's terminal, and not the Matlab console:
+%      $ pip install git+https://github.com/sylvia-dee/PRYSM.git
+%   4. Within the Matlab console, initialize your Matlab session's Python 
+%      environment using the version of Python that contains PRYSM. Do this
+%      using the "pyenv" command. For example, if you are using Python 3.7, do:
+%      >> pyenv('Version', '3.7')
+%   After completing this steps, the PRYSM forward models should be ready
+%   to run in DASH.
+%
+%   ABSTRACT SUPERCLASS:
+%   The individual PRYSM forward model classes inherit from the abstract
+%   superclass PSM.prysm.package. The main purpose of this superclass is to
+%   hold the Github information for the PRYSM forward models, as all the
+%   PRYSM forward models are located in the same Github repository.
 % ----------
 % Abstract Superclass:
 %   package     - Holds repository information for models in the PRYSM suite
