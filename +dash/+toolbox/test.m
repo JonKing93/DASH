d9b6537e9f500978cd376ba24d36bccc59514666
Jonathan King <jonking93@gmail.com>
2023-02-11 22:10:08 -0700

    Move tests driver to dash.toolbox

--- +dash/+toolbox/test.m
+++ +dash/+toolbox/test.m
@@ -1,12 +1,12 @@
-function[] = testToolbox
-%% dash.testToolbox  Runs unit tests for the entire DASH toolbox
+function[] = test
+%% dash.toolbox.test  Runs unit tests for the entire DASH toolbox
 % ----------
-%   dash.testToolbox
+%   dash.toolbox.test
 %   Runs the unit tests for the toolbox. If successful, exits silently. If
 %   unsuccessful, throws an error at the first failed test.
 % ----------
 %
-% <a href="matlab:dash.doc('dash.testToolbox')">Documentation Page</a>
+% <a href="matlab:dash.doc('dash.toolbox.test')">Documentation Page</a>
 
 dash.tests;
 gridMetadata.tests;
