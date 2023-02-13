294d4fc2e9058d9847619f1bdd5916657ea7ca7d
Jonathan King <jonking93@gmail.com>
2023-02-12 21:38:55 -0700

    Revert "Move tests driver to dash.toolbox"
    This reverts commit d9b6537e9f500978cd376ba24d36bccc59514666.

--- +dash/testToolbox.m
+++ +dash/testToolbox.m
@@ -1,12 +1,12 @@
-function[] = test
-%% dash.toolbox.test  Runs unit tests for the entire DASH toolbox
+function[] = testToolbox
+%% dash.testToolbox  Runs unit tests for the entire DASH toolbox
 % ----------
-%   dash.toolbox.test
+%   dash.testToolbox
 %   Runs the unit tests for the toolbox. If successful, exits silently. If
 %   unsuccessful, throws an error at the first failed test.
 % ----------
 %
-% <a href="matlab:dash.doc('dash.toolbox.test')">Documentation Page</a>
+% <a href="matlab:dash.doc('dash.testToolbox')">Documentation Page</a>
 
 dash.tests;
 gridMetadata.tests;
